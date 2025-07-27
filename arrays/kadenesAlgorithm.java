c1d3871523aa37ac6f990938e3b87e09657c1e0f
Muhammed <faraanshaik19@gmail.com>
2025-07-27 23:59:30 +0530

    kadenes algorithm commit

--- arrays/kadenesAlgorithm.java
+++ arrays/kadenesAlgorithm.java
@@ -1,5 +1,21 @@
 package arrays;
 
 public class kadenesAlgorithm {
-    
+    public int maxSubArray(final int[] A) {
+
+        int N=A.length;
+        int ans=Integer.MIN_VALUE;
+        int sum=0;
+
+        for(int i=0; i<N; i++){
+            if(sum<0){
+                sum=0;
+            }
+            sum+=A[i];
+            if(sum>ans){
+                ans=sum;
+            }
+        }
+        return ans;
+    }
 }
