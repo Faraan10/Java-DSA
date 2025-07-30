a774389b9b0ad890d8cd284b9be96fa7175d4e36
Muhammed <faraanshaik19@gmail.com>
2025-07-30 23:40:41 +0530

    Single Number commit

--- bit_manipulation/SingleNumber.java
+++ bit_manipulation/SingleNumber.java
@@ -2,4 +2,13 @@ package bit_manipulation;
 
 public class SingleNumber {
     
+    public int getNumber(int[] arr){
+
+        int N=arr.length;
+        int no=0;
+        for(int i=0; i<N; i++){
+            no^=arr[i];
+        }
+        return no;
+    }
 }
