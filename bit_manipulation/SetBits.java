54ba6dce644ba304c6d1409866705dd9dfe444ad
Muhammed <faraanshaik19@gmail.com>
2025-07-31 00:04:14 +0530

    no of set bits commit

---
+++ bit_manipulation/SetBits.java
@@ -0,0 +1,15 @@
+package bit_manipulation;
+
+public class SetBits {
+    public int numSetBits(int A) {
+
+        int count=0;
+
+        for(int i=0; i<32; i++){
+            if((A & (1<<i))!=0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
