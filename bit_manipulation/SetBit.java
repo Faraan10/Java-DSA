51b2f2407edc7fc4198f7640c0dd32b0b1ea3697
Muhammed <faraanshaik19@gmail.com>
2025-07-31 21:42:12 +0530

    time complexity commit

--- bit_manipulation/SetBit.java
+++ bit_manipulation/SetBit.java
@@ -1,9 +1,10 @@
 package bit_manipulation;
 
 public class SetBit {
+    // TC: O(N)
+    // SC: O(1)
     public int solve(int A, int B) {
 
-        int ans=0;
         int a=0;
         int b=0;
         for(int i=0; i<32; i++){
