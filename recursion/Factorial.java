1abbbee1b3b1567ca6a874673c83a4423463a2af
Muhammed <faraanshaik19@gmail.com>
2025-08-05 19:17:27 +0530

    factorial recursion commit

--- recursion/Factorial.java
+++ recursion/Factorial.java
@@ -1,5 +1,12 @@
 package recursion;
 
 public class Factorial {
-    
+    // TC: O(N)
+    public int solve(int A) {
+
+        if(A==1){
+            return 1;
+        }
+        return A*solve(A-1);
+    }
 }
