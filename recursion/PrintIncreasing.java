3dcd35532497c0929c72bb3926c6899167e59c5e
Muhammed <faraanshaik19@gmail.com>
2025-08-06 19:51:49 +0530

    printIncreasing commit

---
+++ recursion/PrintIncreasing.java
@@ -0,0 +1,25 @@
+package recursion;
+
+public class PrintIncreasing{
+
+    public static void print(int N){
+
+        if(N==1){
+            System.out.print(1+" ");
+            return;
+        }
+
+        print(N-1);
+        System.out.print(N+" ");
+    }
+    public static void solve(int A){
+
+        print(5);
+        
+    }
+
+    public static void main(String[] args){
+
+        solve(5);
+    }
+}
