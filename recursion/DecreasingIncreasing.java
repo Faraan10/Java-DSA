5c9542708f682a5126a9fe41bd685bc326c73e1e
Muhammed <faraanshaik19@gmail.com>
2025-08-08 23:36:08 +0530

    commit

---
+++ recursion/DecreasingIncreasing.java
@@ -0,0 +1,21 @@
+package recursion;
+
+public class DecreasingIncreasing {
+    
+    public void print(int N){
+
+        if(N==0){
+            return;
+        }
+
+        System.out.print(N+" ");
+        print(N-1);
+        System.out.print(N+" ");
+    }
+
+    public void DecThenInc(int A) {
+
+        print(A);
+        System.out.println();
+    }
+}
