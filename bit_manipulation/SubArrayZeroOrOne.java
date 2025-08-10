9cc9b59496c59cd246964eaea06c2fd1c108c8eb
Muhammed <faraanshaik19@gmail.com>
2025-08-10 20:55:34 +0530

    commit

---
+++ bit_manipulation/SubArrayZeroOrOne.java
@@ -0,0 +1,27 @@
+package bit_manipulation;
+
+public class SubArrayZeroOrOne {
+    public int subarraysWithOR1(int[] A) {
+
+        int n = A.length;
+        long total = (long) n * (n + 1) / 2;  // total subarrays
+
+        long zeroSubarrays = 0;
+        long zeroCount = 0;
+
+        for (int num : A) {
+            if (num == 0) {
+                zeroCount++;
+            } else {
+                // end of zero block
+                zeroSubarrays += zeroCount * (zeroCount + 1) / 2;
+                zeroCount = 0;
+            }
+        }
+        // last zero block if any
+        zeroSubarrays += zeroCount * (zeroCount + 1) / 2;
+
+        return (int) (total - zeroSubarrays);
+    }
+    
+}
