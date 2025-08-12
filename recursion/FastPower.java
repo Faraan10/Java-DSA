780d6e44bee7ecb999b9e8b27ec8e6e211abfa29
Muhammed <faraanshaik19@gmail.com>
2025-08-13 01:29:40 +0530

    fast power commit

--- recursion/FastPower.java
+++ recursion/FastPower.java
@@ -1,9 +1,9 @@
 package recursion;
 
 public class FastPower {
-    // TC: O(log N)  every time we divide it by 2 so N/2, N/4, N/8... 
-    // So, O(lon N) will reach 1 after about logN steps
-    // SC: O(N) recursion stack space
+    // TC: O(log B)  every time we divide it by 2 so N/2, N/4, N/8... 
+    // So, O(log B) will reach 1 after about logN steps
+    // SC: O(log B) recursion stack space
     public long power(int A, int B) {
 
         if(B==0){
@@ -12,7 +12,7 @@ public class FastPower {
 
         // by using this below variable we will be computing the value 
         // only once and use it inside the return recursive statements
-        // avoiding repetitive calls so TC: O(log N) 
+        // avoiding repetitive calls so TC: O(log B) 
         long half= power(A, B/2);
 
         // if B is even then just multiplying 2 halves
