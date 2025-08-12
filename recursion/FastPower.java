50ad3d8a756d09a638fa7844b2fddd8fa5d1adf9
Muhammed <faraanshaik19@gmail.com>
2025-08-13 01:20:37 +0530

    fast power commit

--- recursion/FastPower.java
+++ recursion/FastPower.java
@@ -1,7 +1,9 @@
 package recursion;
 
 public class FastPower {
-    
+    // TC: O(log N)  every time we divide it by 2 so N/2, N/4, N/8... 
+    // So, O(lon N) will reach 1 after about logN steps
+    // SC: O(N) recursion stack space
     public long power(int A, int B) {
 
         if(B==0){
