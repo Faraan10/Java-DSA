86d2caac0fd4f472fc38c801185d6f2aa5298c56
Muhammed <faraanshaik19@gmail.com>
2025-08-17 21:47:48 +0530

    problem commit

--- hashMap/CheckPairSum.java
+++ hashMap/CheckPairSum.java
@@ -1,21 +1,40 @@
 package hashMap;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class CheckPairSum {
     
     // TC: O(B)
     // SC: O(B) worst case as we are using hashmap
-    public int solve(int A, int[] B) {
+    // public int solve(int A, int[] B) {
 
-        HashMap<Integer, Integer> hm=new HashMap<>();
+    //     HashMap<Integer, Integer> hm=new HashMap<>();
+    //     int N=B.length;
+
+    //     for(int i=0; i<N; i++){
+    //         if(hm.containsKey(A-B[i])){
+    //             return 1;
+    //         }else{
+    //             hm.put(B[i], 1);
+    //         }
+    //     }
+    //     return 0;
+    // }
+
+    // best suited for this problem using HashSet
+
+    public int solve(int A, int[] B) {
+        // TC: O(B)
+        // SC: O(B) worst case as we are using hashset
+        HashSet<Integer> hs=new HashSet<>();
         int N=B.length;
 
         for(int i=0; i<N; i++){
-            if(hm.containsKey(A-B[i])){
+            if(hs.contains(A-B[i])){
                 return 1;
             }else{
-                hm.put(B[i], 1);
+                hs.add(B[i]);
             }
         }
         return 0;
