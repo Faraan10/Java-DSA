c8137497d090b5f6fa5ae402d64e52951b3c15e1
Muhammed <faraanshaik19@gmail.com>
2025-08-19 22:59:57 +0530

    problem commit

--- hashing/SubArraySumEqualsK.java
+++ hashing/SubArraySumEqualsK.java
@@ -1,19 +1,38 @@
 package hashing;
 
 public class SubArraySumEqualsK {
-
+    // Bruteforce
     // TC: O(N^3) as using 3 loops 3*N worst case
     // SC: O(1) no extra space used
+    // public int solve(int[] A, int B) {
+
+    //     int N=A.length;
+    //     int count=0;
+    //     for(int i=0; i<N; i++){
+    //         for(int j=i; j<N; j++){
+    //             int sum=0;
+    //             for(int k=i; k<=j; k++){
+    //                 sum+=A[k];
+    //             }
+    //             if(sum==B){
+    //                 count++;
+    //             }
+    //         }
+    //     }
+    //     return count;
+    // }
+
+    // little optimized using running sum 
+    // TC: O(N^2)
+    // SC: O(1)
     public int solve(int[] A, int B) {
 
         int N=A.length;
         int count=0;
         for(int i=0; i<N; i++){
+            int sum=0;
             for(int j=i; j<N; j++){
-                int sum=0;
-                for(int k=i; k<=j; k++){
-                    sum+=A[k];
-                }
+                sum+=A[j];
                 if(sum==B){
                     count++;
                 }
