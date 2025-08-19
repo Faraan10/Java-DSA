16ea388f4e3ecf899f52db5dd2d12ac8c756b6a7
Muhammed <faraanshaik19@gmail.com>
2025-08-19 23:17:23 +0530

    problem commit

--- hashing/SubArraySumEqualsK.java
+++ hashing/SubArraySumEqualsK.java
@@ -1,5 +1,7 @@
 package hashing;
 
+import java.util.HashMap;
+
 public class SubArraySumEqualsK {
     // Bruteforce
     // TC: O(N^3) as using 3 loops 3*N worst case
@@ -25,17 +27,46 @@ public class SubArraySumEqualsK {
     // little optimized using running sum 
     // TC: O(N^2)
     // SC: O(1)
-    public int solve(int[] A, int B) {
+    // public int solve(int[] A, int B) {
 
+    //     int N=A.length;
+    //     int count=0;
+    //     for(int i=0; i<N; i++){
+    //         int sum=0;
+    //         for(int j=i; j<N; j++){
+    //             sum+=A[j];
+    //             if(sum==B){
+    //                 count++;
+    //             }
+    //         }
+    //     }
+    //     return count;
+    // }
+
+
+    public int solve(int[] A, int B) {
+        
         int N=A.length;
+        HashMap<Integer, Integer> hm=new HashMap<>();
+        hm.put(0,1);
+
+        int prefixSum=0;
         int count=0;
+
         for(int i=0; i<N; i++){
-            int sum=0;
-            for(int j=i; j<N; j++){
-                sum+=A[j];
-                if(sum==B){
-                    count++;
-                }
+
+            prefixSum+=A[i];
+
+            if(hm.containsKey(prefixSum-B)){
+                count+=hm.get(prefixSum-B);
+            }
+
+            if(hm.containsKey(prefixSum)){
+                int value=hm.get(prefixSum);
+                int newValue=value+1;
+                hm.put(prefixSum, newValue);
+            }else{
+                hm.put(prefixSum, 1);
             }
         }
         return count;
