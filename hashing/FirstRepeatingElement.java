9eb0f6d21a6ced42888fba4a6551f54ad60c289c
Muhammed <faraanshaik19@gmail.com>
2025-08-20 23:17:04 +0530

    commit

---
+++ hashing/FirstRepeatingElement.java
@@ -0,0 +1,20 @@
+package hashing;
+
+import java.util.HashSet;
+
+public class FirstRepeatingElement {
+    public int solve(int[] A) {
+
+        int N=A.length;
+        int ans=-1;
+        HashSet<Integer> hs=new HashSet<>();
+
+        for(int i=N-1; i>=0; i--){
+            if(hs.contains(A[i])){
+                ans=A[i];
+            }
+            hs.add(A[i]);
+        }
+        return ans;
+    }
+}
