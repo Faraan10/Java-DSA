36907a40aa9d4307a04b1dd7482b77557309d6e9
Muhammed <faraanshaik19@gmail.com>
2025-08-21 23:33:10 +0530

    problem commit

--- arrays/kadenesAlgorithm.java
+++ arrays/kadenesAlgorithm.java
@@ -20,4 +20,37 @@ public class kadenesAlgorithm {
     //     }
     //     return ans;
     // }
+
+
+    // in the below approach I am also including start and end 
+    // index of the subarray
+    public int[] maxSubArray(int[] arr){
+        
+        int temp_start=0;
+        int start_idx=0;
+        int end_idx=0;
+        int sum=Integer.MIN_VALUE;
+        int ans=0;
+        int N=arr.length;
+        int[] array=new int[3];
+
+        for(int i=0; i<N; i++){
+
+            if(ans<=0){
+                ans=0;
+                temp_start=i;
+            }
+            ans+=arr[i];
+            if(ans>sum){
+                sum=ans;
+                start_idx=temp_start;
+                end_idx=i;
+            }
+        }
+        array[0]=start_idx;
+        array[1]=end_idx;
+        array[2]=sum;
+
+        return array;
+    }
 }
