4d46408cdf5acbc9e1ca22ebf5719e7c0be8c4c7
Muhammed <faraanshaik19@gmail.com>
2025-08-25 23:02:35 +0530

    commit

---
+++ recursion/MaxOfArray.java
@@ -0,0 +1,18 @@
+package recursion;
+
+public class MaxOfArray {
+    
+    public int Max(int[] arr, int start, int end, int high){
+
+        if(start>end){
+            return high;
+        }
+        high=Math.max(high, arr[start]);
+        return Max(arr, start+1, end, high);
+    }
+
+    public int getMax(int[] A) {
+
+        return Max(A, 0, A.length-1, Integer.MIN_VALUE);
+    }
+}
