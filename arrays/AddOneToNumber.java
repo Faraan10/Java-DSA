d769847e89e869487bc850d8897817840b9aeddb
Muhammed <faraanshaik19@gmail.com>
2025-08-28 23:53:52 +0530

    problem commit

---
+++ arrays/AddOneToNumber.java
@@ -0,0 +1,36 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class AddOneToNumber {
+    
+    
+    public int[] plusOne(int[] A) {
+
+        int N=A.length;
+
+        int j=0;
+        while(j<N && A[j]==0){
+            j++;
+        }
+
+        if(j==N){
+            return new int[] {1};
+        }
+
+        for(int i=N-1; i>=j; i--){
+            if(A[i]<9){
+                A[i]++;
+                return Arrays.copyOfRange(A,j,N);
+            }
+            A[i]=0;
+        }
+
+        int[] arr=new int[N+1-j];
+        for(int i=j; i<A.length; i++){
+            arr[i-j+1]=A[i];
+        }
+        arr[0]=1;
+        return arr;
+    }
+}
