b82fdcdaacb864adf4bb85d6f6b79bae1c75522b
Muhammed <faraanshaik19@gmail.com>
2025-08-30 22:00:09 +0530

    commit

---
+++ BinarySearch/SearchInRange.java
@@ -0,0 +1,24 @@
+package BinarySearch;
+
+public class SearchInRange {
+    
+    public int[] searchRange(final int[] A, int B) {
+
+        int N=A.length;
+        int leftMost=-1;
+        int rightMost=-1;
+        for(int i=0; i<N; i++){
+            if(A[i]==B){
+                rightMost=i;
+            }
+        }
+
+        for(int i=N-1; i>=0; i--){
+            if(A[i]==B){
+                leftMost=i;
+            }
+        }
+
+        return new int[] {leftMost, rightMost};
+    }
+}
