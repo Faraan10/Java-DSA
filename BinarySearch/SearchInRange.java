40d2dfdbd95deb521bbad33e23d7d30452c11278
Muhammed <faraanshaik19@gmail.com>
2025-08-30 22:15:16 +0530

    optimized commit

--- BinarySearch/SearchInRange.java
+++ BinarySearch/SearchInRange.java
@@ -23,4 +23,57 @@ public class SearchInRange {
 
         return new int[] {leftMost, rightMost};
     }
+
+    
+    public static int leftMostIndex(int[] arr, int K){
+
+        int low=0;
+        int high=arr.length-1;
+        int leftMost=-1;
+
+        while(low<=high){
+
+            int mid=low+(high-low)/2;
+
+            if(arr[mid]==K){
+                leftMost=mid;
+                high=mid-1;
+            }else if(arr[mid]>K){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return leftMost;
+    }
+
+    public static int rightMostIndex(int[] arr, int K){
+
+        int low=0;
+        int high=arr.length-1;
+        int rightMost=-1;
+
+        while(low<=high){
+
+            int mid=low+(high-low)/2;
+
+            if(arr[mid]==K){
+                rightMost=mid;
+                low=mid+1;
+            }else if(arr[mid]>K){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return rightMost;
+    }
+
+    public int[] searchInRange(final int[] A, int B) {
+
+        int first=leftMostIndex(A, B);
+        int last=rightMostIndex(A, B);
+
+        return new int[] {first, last};
+    }
 }
