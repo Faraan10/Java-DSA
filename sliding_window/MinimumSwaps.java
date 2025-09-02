b45befb777abf9113dc9423ebb9d1f4a442454d6
Muhammed <faraanshaik19@gmail.com>
2025-09-02 23:26:30 +0530

    problem commit

---
+++ sliding_window/MinimumSwaps.java
@@ -0,0 +1,44 @@
+package sliding_window;
+
+public class MinimumSwaps {
+
+    public int solve(int[] A, int B) {
+
+        int N=A.length;
+        int count=0;
+
+        for(int i=0; i<N; i++){
+            if(A[i]<=B){
+                count++;
+            }
+        }
+
+        if(count<=1){
+            return 0;
+        }
+
+        int range=0;
+        for(int i=0; i<count; i++){
+            if(A[i]<=B){
+                range++;
+            }
+        }
+
+        int i=1; 
+        int j=count;
+        int ans=range;
+        while(j<N){
+            if(A[i-1]<=B){ // if good number is removed -1 
+                range=range-1;
+            }
+            if(A[j]<=B){ // if good number is being added doing +1
+                range=range+1;
+            }
+            ans=Math.max(ans, range);
+            i++;
+            j++;
+        }
+
+        return count-ans;
+    }
+}
