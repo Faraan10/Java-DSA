dbc781c51b3958d1db7c96398fc1e6f55095a7e6
Muhammed <faraanshaik19@gmail.com>
2025-09-04 23:58:30 +0530

    Kth element commit

--- linkedList/KthElement.java
+++ linkedList/KthElement.java
@@ -1,5 +1,30 @@
 package linkedList;
 
+
+class ListNode{
+    public int val;
+    public ListNode next;
+
+    ListNode(int x){
+        val = x; 
+        next = null; 
+    }
+}
+
 public class KthElement {
     
+    public int solve(ListNode A, int B) {
+
+        ListNode temp=A;
+        int index=0;
+
+        while(temp != null){
+            if(index==B){
+                return temp.val;
+            }
+            temp=temp.next;
+            index++;
+        }
+        return -1;
+    }
 }
