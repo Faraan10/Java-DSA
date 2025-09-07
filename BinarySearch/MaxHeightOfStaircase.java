2f7e0bc5b11c5e5cba4686b1632735c18637d3af
Muhammed <faraanshaik19@gmail.com>
2025-09-08 02:13:04 +0530

    problem commit

---
+++ BinarySearch/MaxHeightOfStaircase.java
@@ -0,0 +1,25 @@
+package BinarySearch;
+
+public class MaxHeightOfStaircase {
+    
+
+    public int solve(int A) {
+
+        long low=1;
+        long high=A;
+        long ans=0;
+
+        while(low<=high){
+
+            long mid=low+(high-low)/2;
+
+            if((mid*(mid+1)/2)<=A){
+                ans=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return (int)ans;
+    }
+}
