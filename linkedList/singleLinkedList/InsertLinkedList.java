84328c37101c60df07f82199a569554d14e8f2f3
Muhammed <faraanshaik19@gmail.com>
2025-09-09 23:40:37 +0530

    problem commit

---
+++ linkedList/singleLinkedList/InsertLinkedList.java
@@ -0,0 +1,44 @@
+package linkedList.singleLinkedList;
+
+public class InsertLinkedList {
+    
+    public ListNode solve(ListNode A, int B, int C) {
+
+        ListNode temp=A;
+        ListNode newNode=new ListNode(B);
+        if(C==0){
+            newNode.next=temp;
+            return newNode;
+        }
+
+        if(A==null){
+            return newNode;
+        }
+
+        int idx=0;
+        temp=A;
+        while(temp!=null){
+            temp=temp.next;
+            idx++;
+        }
+        if(C>=idx){
+            temp=A;
+            while(temp.next!=null){
+                temp=temp.next;
+            }
+            temp.next=newNode;
+            return A;
+        }
+
+        idx=0;
+        temp=A;
+        while(idx<C-1){
+            temp=temp.next;
+            idx++;
+        }
+        newNode.next=temp.next;
+        temp.next=newNode;
+
+        return A;
+    }
+}
