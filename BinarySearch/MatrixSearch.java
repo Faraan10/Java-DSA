2c9589a9da48ca8b6c08c93279793ce90b8ed913
Muhammed <faraanshaik19@gmail.com>
2025-09-11 00:43:46 +0530

    problem commit

--- BinarySearch/MatrixSearch.java
+++ BinarySearch/MatrixSearch.java
@@ -5,18 +5,40 @@ public class MatrixSearch {
     // TC: O(N*M) i loop till N-1 and j loop till M-1 N*M
     // SC: O(1)
     // bruteforce approach
-    public int searchMatrix(int[][] A, int B) {
+    // public int searchMatrix(int[][] A, int B) {
 
-        int N=A.length;
-        int M=A[0].length;
+    //     int N=A.length;
+    //     int M=A[0].length;
 
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                if(A[i][j]==B){
-                    return 1;
-                }
-            }
-        }
-        return 0;
-    }
+    //     for(int i=0; i<N; i++){
+    //         for(int j=0; j<M; j++){
+    //             if(A[i][j]==B){
+    //                 return 1;
+    //             }
+    //         }
+    //     }
+    //     return 0;
+    // }
+
+    // TC: O(N)
+    // SC: O(1)
+    // optimized method only can be used when the matrix is sorted
+    // public int searchMatrix(int[][] A, int B) {
+
+    //     int N=A.length;
+    //     int M=A[0].length;
+
+    //     int i=0;
+    //     int j=M-1;
+    //     while(i<N && j>=0){
+    //         if(A[i][j]==B){
+    //             return 1;
+    //         }else if(A[i][j]<B){
+    //             i++;
+    //         }else{
+    //             j--;
+    //         }
+    //     }
+    //     return 0;
+    // }
 }
