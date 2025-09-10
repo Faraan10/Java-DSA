8b6e3bc478dca5943f3d619c7b2422fc284d83bb
Muhammed <faraanshaik19@gmail.com>
2025-09-11 00:55:46 +0530

    problem commit

--- BinarySearch/MatrixSearch.java
+++ BinarySearch/MatrixSearch.java
@@ -41,4 +41,31 @@ public class MatrixSearch {
     //     }
     //     return 0;
     // }
+
+
+
+    public int searchMatrix(int[][] A, int B) {
+
+        int N=A.length;
+        int M=A[0].length;
+
+        int low=0;
+        int high=N*M-1;
+
+        while(low<=high){
+
+            int mid=low+(high-low)/2;
+
+            int row=mid/M;
+            int col=mid%M;
+            if(A[row][col]==B){
+                return 1;
+            }else if(A[row][col]<B){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return 0;
+    }
 }
