56f058c309b12f5991061cf20f44b9b634e8829f
Muhammed <faraanshaik19@gmail.com>
2025-09-12 01:36:29 +0530

    problem commit

---
+++ stacks/PassingGame.java
@@ -0,0 +1,24 @@
+package stacks;
+
+import java.util.Stack;
+
+public class PassingGame {
+    
+  
+    public int solve(int A, int B, int[] C) {
+
+        Stack<Integer> st=new Stack<Integer>();
+        int N=C.length;
+        int i=0;
+        st.push(B);
+        while(i<N){
+            if(C[i] != 0){
+                st.push(C[i]);
+            }else{
+                st.pop();
+            }
+            i++;
+        }
+        return st.peek();
+    }
+}
