7b0a3e72af4056e29d7d71a9273211270919d834
Muhammed <faraanshaik19@gmail.com>
2025-09-13 01:34:44 +0530

    problem commit

---
+++ stacks/BalancedParenthesis.java
@@ -0,0 +1,62 @@
+package stacks;
+
+import java.util.Stack;
+
+public class BalancedParenthesis {
+    
+    public int solve(String A) {
+
+        int N=A.length();
+        Stack<Character> st=new Stack<>();
+
+        int i=0;
+        while(i<N){
+
+            if(opening(A.charAt(i))){
+                st.push(A.charAt(i));
+            }else{
+                if(st.isEmpty()){
+                    return 0;
+                }
+                char peek=st.peek();
+                if(closing(peek, A.charAt(i))){
+                    st.pop();
+                }else{
+                    return 0;
+                }
+            }
+            i++;
+        }
+
+        if(st.isEmpty()){
+            return 1;
+        }
+        return 0;
+    }
+
+    public static boolean opening(char A){
+
+        if(A=='{'){
+            return true;
+        }else if(A=='['){
+            return true;
+        }else if(A=='('){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static boolean closing(char peek, char A){
+
+        if(peek=='[' && A==']'){
+            return true;
+        }else if( peek=='{' && A=='}'){
+            return true;
+        }else if(peek=='(' && A==')'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
