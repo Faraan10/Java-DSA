97ff0bc25691b27b017dddf33565f2d1881d38f8
Muhammed <faraanshaik19@gmail.com>
2025-09-14 11:04:07 +0530

    problem commit

--- recursion/FirstIndex.java
+++ recursion/FirstIndex.java
@@ -1,5 +1,23 @@
 package recursion;
 
 public class FirstIndex {
-    
+    public int FirstIndex(int[] A, int B) {
+
+        return findIndex(A, B, 0);
+    }
+
+     private int findIndex(int[] A, int B, int index) {
+        // Base case: if we've checked all elements
+        if (index == A.length) {
+            return -1;
+        }
+        
+        // If element found at current index
+        if (A[index] == B) {
+            return index;
+        }
+        
+        // Recursive case: check the next index
+        return findIndex(A, B, index + 1);
+    }
 }
