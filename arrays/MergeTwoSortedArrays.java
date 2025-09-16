7444633ab67ef48d725f519c6a054112ce395536
Muhammed <faraanshaik19@gmail.com>
2025-09-16 23:02:30 +0530

    problem commit

---
+++ arrays/MergeTwoSortedArrays.java
@@ -0,0 +1,40 @@
+package arrays;
+
+public class MergeTwoSortedArrays {
+    
+    // TC: O(N)
+    // SC: O(N)
+    public int[] solve(final int[] A, final int[] B) {
+
+        int N=A.length;
+        int M=B.length;
+        int[] arr=new int[N+M];
+
+        int i=0;
+        int j=0;
+        int k=0;
+        while(j<N && k<M){
+            if(A[j]<B[k]){
+                arr[i]=A[j];
+                j++;
+            }else{
+                arr[i]=B[k];
+                k++;
+            }
+            i++;
+        }
+
+        while(j<N){
+            arr[i]=A[j];
+            i++;
+            j++;
+        }
+
+        while(k<M){
+            arr[i]=B[k];
+            i++;
+            k++;
+        }
+        return arr;
+    }
+}
