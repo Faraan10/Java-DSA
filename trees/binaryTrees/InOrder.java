23dd718278b0574c570ff4da49407dd1328f4eb2
Muhammed <faraanshaik19@gmail.com>
2025-09-22 00:54:46 +0530

    problems commit

---
+++ trees/binaryTrees/InOrder.java
@@ -0,0 +1,41 @@
+package trees.binaryTrees;
+
+import java.util.ArrayList;
+
+class TreeNode{
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x){
+        val=x;
+        left=null;
+        right=null;
+    }
+}
+
+public class InOrder {
+    
+    // TC: O(N)
+    // SC: O(N)
+    public static ArrayList<Integer> solve(TreeNode A){
+
+        ArrayList<Integer> ans=new ArrayList<Integer>();
+
+        InOrderTraverse(A, ans);
+
+        return ans;
+    }
+
+    public static void InOrderTraverse(TreeNode node, ArrayList<Integer> list){
+
+        if(node==null){
+            return;
+        }
+
+        InOrderTraverse(node.left, list);
+        list.add(node.val);
+        InOrderTraverse(node.right, list);
+    }
+}
