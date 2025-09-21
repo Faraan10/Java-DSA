b3a7215507e22a9ff1b11e83a27558a176d717f0
Muhammed <faraanshaik19@gmail.com>
2025-09-21 21:37:56 +0530

    problem commit

--- trees/RightViewBinaryTree.java
+++ trees/RightViewBinaryTree.java
@@ -21,32 +21,30 @@ class TreeNode{
 
 public class RightViewBinaryTree {
     
+    // TC: O(N)
+    // SC: O(N)
     public ArrayList<Integer> solve(TreeNode A){
 
         Queue<TreeNode> que=new LinkedList<>();
         ArrayList<Integer> ans=new ArrayList<>();
         que.add(A); // adding first root node
-        TreeNode required=null;
 
         while(!que.isEmpty()){
 
-            TreeNode remove=que.remove();
-            
             int size=que.size();
+            TreeNode remove=null;
 
             for(int i=0; i<size; i++){
-
+                
+                remove=que.remove();
                 if(remove.left != null){
                     que.add(remove.left);
                 }
                 if(remove.right != null){
                     que.add(remove.right);
                 }
-                if(i == size-1){
-                    required=remove;
-                }
             }
-            ans.add(required.val);
+            ans.add(remove.val);
         }
         return ans;
     }
