f9e81c14a5c70d9b1db6951f7a80d2b95e0399e9
Muhammed <faraanshaik19@gmail.com>
2025-09-29 23:21:16 +0530

    commit

---
+++ recursion/LastIndex.java
@@ -0,0 +1,22 @@
+package recursion;
+
+public class LastIndex {
+    
+    public int LastIndex(int[] A, int B) {
+
+        return helper(A, B, A.length-1);
+    }
+
+    public static int helper(int[] arr, int B, int N){
+
+        if(N<0){
+            return -1;
+        }
+
+        if(arr[N]==B){
+            return N;
+        }
+
+        return helper(arr, B, N-1);
+    }
+}
