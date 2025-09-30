3eb8a695e5a087fe8a48b1d53b280083083035de
Muhammed <faraanshaik19@gmail.com>
2025-09-30 22:48:11 +0530

    commit

---
+++ modularArithmetic/modSum.java
@@ -0,0 +1,31 @@
+package modularArithmetic;
+
+public class modSum {
+    
+    static final int MOD = 1000000007;
+    public int solve(int[] A) {
+
+        int[] freq = new int[1001];
+
+        // Step 1: Build frequency array
+        for (int num : A) {
+            freq[num]++;
+        }
+
+        long ans = 0;
+
+        // Step 2: Calculate contributions
+        for (int a = 1; a <= 1000; a++) {
+            if (freq[a] == 0) continue;
+
+            for (int b = 1; b <= 1000; b++) {
+                if (freq[b] == 0) continue;
+
+                ans += (long)(a % b) * freq[a] * freq[b];
+                if (ans >= (long)MOD * MOD) ans %= MOD; // to avoid overflow
+            }
+        }
+
+        return (int)(ans % MOD);
+    }
+}
