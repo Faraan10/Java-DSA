b8943b170e68ddb8bb783729b8d3983121123166
Muhammed <faraanshaik19@gmail.com>
2025-10-03 22:09:51 +0530

    commit

--- modularArithmetic/PowerFunction.java
+++ modularArithmetic/PowerFunction.java
@@ -2,12 +2,12 @@ package modularArithmetic;
 
 public class PowerFunction {
     
-    public int pow(int A, int B, int C) {
+    public int power(int A, int B, int C) {
        
         // TC: O(N)
         // SC: O(N)
          if (B == 0) return 1%C;
-        long half = pow(A, B / 2, C);
+        long half = power(A, B / 2, C);
         long result = (half * half) % C;
         if (B % 2 != 0) {
             result = (result * (A%C)) % C;
