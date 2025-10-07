27f7600e4573e3c0b787d59369871369b8c2f502
Muhammed <faraanshaik19@gmail.com>
2025-10-07 23:57:14 +0530

    commit

---
+++ maths/checkpairSum.java
@@ -0,0 +1,22 @@
+package maths;
+
+import java.util.HashSet;
+
+public class checkpairSum {
+    
+    public int solve(int A, int[] B) {
+
+        HashSet<Integer> hs=new HashSet<>();
+        int N=B.length;
+
+
+        for(int i=0; i<N; i++){
+            if(hs.contains(A-B[i])){
+                return 1;
+            }else{
+                hs.add(B[i]);
+            }
+        }
+        return 0;
+    }
+}
