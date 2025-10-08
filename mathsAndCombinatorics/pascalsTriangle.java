2ddae320d84e9c42e90feda7a1885684ff113765
Muhammed <faraanshaik19@gmail.com>
2025-10-09 01:43:33 +0530

    commit

---
+++ mathsAndCombinatorics/pascalsTriangle.java
@@ -0,0 +1,19 @@
+package mathsAndCombinatorics;
+
+public class pascalsTriangle {
+    
+    public int[][] solve(int A) {
+
+        int[][] matrix=new int[A][A];
+
+        for(int i=0; i<A; i++){
+            matrix[i][0]=1;
+            matrix[i][i]=1;
+            for(int j=1; j<i; j++){
+                matrix[i][j]=matrix[i-1][j]+matrix[i-1][j-1];
+            }
+        }
+
+        return matrix;
+    }
+}
