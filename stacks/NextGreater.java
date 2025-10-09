7cb2e48616b711c60c3000b96154a7f98abd91c6
Muhammed <faraanshaik19@gmail.com>
2025-10-09 23:35:11 +0530

    commit

---
+++ stacks/NextGreater.java
@@ -0,0 +1,35 @@
+package stacks;
+
+import java.util.Stack;
+
+public class NextGreater {
+    
+
+    public int[] nextGreater(int[] A) {
+
+        int N=A.length;
+        Stack<Integer> st=new Stack<>();
+
+        int[] arr=new int[N];
+        st.push(N-1);
+        arr[N-1]=-1;
+
+        for(int i=N-2; i>=0; i--){
+            if(!st.isEmpty() && A[st.peek()] > A[i]){
+                arr[i]=A[st.peek()];
+                st.push(i);
+            }else{
+                while(!st.isEmpty() && A[st.peek()]<=A[i]){
+                    st.pop();
+                }
+                if(!st.isEmpty()){
+                    arr[i]=A[st.peek()];   
+                }else{
+                    arr[i]=-1;
+                }
+                st.push(i);
+            }
+        }
+        return arr;
+    }
+}
