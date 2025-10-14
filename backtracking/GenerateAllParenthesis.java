47acd1814086b044e2fec383bea17f6e794ac266
Muhammed <faraanshaik19@gmail.com>
2025-10-14 23:54:43 +0530

    commit

--- backtracking/GenerateAllParenthesis.java
+++ backtracking/GenerateAllParenthesis.java
@@ -1,5 +1,29 @@
 package backtracking;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class GenerateAllParenthesis {
     
+    public String[] generateParenthesis(int A) {
+
+         List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, A);
+        Collections.sort(result);
+        return result.toArray(new String[0]);
+    }
+
+    public static void backtrack(List<String> result, String current, int open, int close, int max) {
+        if (current.length() == 2 * max) {
+            result.add(current);
+            return;
+        }
+
+        if (open < max)
+            backtrack(result, current + "(", open + 1, close, max);
+
+        if (close < open)
+            backtrack(result, current + ")", open, close + 1, max);
+    }
 }
