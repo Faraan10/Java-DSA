4b01db9f0f577df2ad69e9c807a89b7a9d9727fd
Muhammed <faraanshaik19@gmail.com>
2025-10-15 20:53:50 +0530

    commit

---
+++ backtracking/GenerateSubsets.java
@@ -0,0 +1,26 @@
+package backtracking;
+
+import java.util.ArrayList;
+
+public class GenerateSubsets {
+    
+    public ArrayList<ArrayList<Integer>> subsets(ArrayList<Integer> A) {
+        // code here
+
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        backtrack(A, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    public static void backtrack(ArrayList<Integer> A, int index, ArrayList<Integer> current, ArrayList<ArrayList<Integer>> result) {
+        // Add current subset to result
+        result.add(new ArrayList<>(current));
+
+        // Explore further elements
+        for (int i = index; i < A.size(); i++) {
+            current.add(A.get(i));              // Choose
+            backtrack(A, i + 1, current, result); // Explore
+            current.remove(current.size() - 1);   // Unchoose (backtrack)
+        }
+    }
+}
