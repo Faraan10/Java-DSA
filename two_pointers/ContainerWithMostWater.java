74840b612073d7c9d91dcac634e5f9c6420633da
Muhammed <faraanshaik19@gmail.com>
2025-10-17 02:04:30 +0530

    commit

---
+++ two_pointers/ContainerWithMostWater.java
@@ -0,0 +1,25 @@
+package two_pointers;
+
+public class ContainerWithMostWater {
+    
+    public int maxArea(int[] A){
+
+        int N=A.length;
+        int l=0;
+        int r=N-1;
+
+        int ans=0;
+        while(l<r){
+
+            int area=(Math.min(A[l], A[r])*(r-l));
+            ans=Math.max(ans, area);
+
+            if(A[l]<=A[r]){
+                l++;
+            }else{
+                r--;
+            }
+        }
+        return ans;
+    }
+}
