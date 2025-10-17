027dae01d5b0239d9a3ec035f00b70edf63b80b1
Muhammed <faraanshaik19@gmail.com>
2025-10-18 01:49:03 +0530

    commit

---
+++ linkedList/singleLinkedList/MiddleElement.java
@@ -0,0 +1,17 @@
+package linkedList.singleLinkedList;
+
+
+class ListNode{
+
+    int x;
+    ListNode next;
+
+    ListNode(int val){
+        x=val;
+        next=null;
+    }
+}
+
+public class MiddleElement {
+    
+}
