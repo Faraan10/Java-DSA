bf93e26cbfa1fa159e2f45b6bb1f1d263f65759f
Muhammed <faraanshaik19@gmail.com>
2025-10-18 02:02:18 +0530

    middle element commit

--- linkedList/singleLinkedList/MiddleElement.java
+++ linkedList/singleLinkedList/MiddleElement.java
@@ -13,5 +13,33 @@ class ListNode{
 }
 
 public class MiddleElement {
+
+    public static int solve(ListNode A){
+
+        if(A==null){
+            return 0;
+        }
+
+        ListNode temp=A;
+        int size=0;
+
+        while(temp!=null){
+            temp=temp.next;
+            size++;
+        }
+
+        temp=A;
+        if(size%2==0){
+            for(int i=0; i<(size/2)-1; i++){
+                temp=temp.next;
+            }
+            return temp.x;
+        }else{
+            for(int i=0; i<(size/2); i++){
+                temp=temp.next;
+            }
+            return temp.x;
+        }
+    }
     
 }
