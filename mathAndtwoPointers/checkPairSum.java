ba591d0265dddf92409f51087b1cd4a5e4010ab2
Muhammed <faraanshaik19@gmail.com>
2025-10-19 23:07:45 +0530

    commit

---
+++ mathAndtwoPointers/checkPairSum.java
@@ -0,0 +1,27 @@
+package mathAndtwoPointers;
+
+import java.util.Arrays;
+
+public class checkPairSum {
+    
+    // TC: O(N log N)
+    // SC: O(1)
+    public int solve(int A, int[] B) {
+
+        int i=0;
+        int j=B.length-1;
+
+        Arrays.sort(B);
+
+        while(i<j){
+            if((long)B[i]+B[j]==A){
+                return 1;
+            }else if((long)B[i]+B[j]>A){
+                j--;
+            }else{
+                i++;
+            }
+        }
+        return 0;
+    }
+}
