1629d238c261a23f3104003b664ea36232629a15
Muhammed <faraanshaik19@gmail.com>
2025-10-20 23:37:48 +0530

    commit

--- linkedList/singleLinkedList/MergeTwoSortedLinkedLists.java
+++ linkedList/singleLinkedList/MergeTwoSortedLinkedLists.java
@@ -2,5 +2,28 @@ package linkedList.singleLinkedList;
 
 public class MergeTwoSortedLinkedLists {
     
-    
+    public ListNode mergeTwoLists(ListNode A, ListNode B) {
+
+        ListNode head=new ListNode(-1);
+        ListNode temp=head; // tail pointer
+
+        while(A!=null && B!=null){
+            if(A.val<=B.val){
+                temp.next=A;
+                A=A.next;
+            }else{
+                temp.next=B;
+                B=B.next;
+            }
+            temp=temp.next;
+        }
+
+        if(A!=null){
+            temp.next=A;
+        }else{
+            temp.next=B;
+        }
+
+        return head.next;
+    }
 }
