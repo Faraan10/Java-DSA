ccbd38c535b38f619fb74c3e9ce31a48cf02e93f
Muhammed <faraanshaik19@gmail.com>
2025-10-22 17:37:52 +0530

    commit

--- backtracking/Permutations.java
+++ backtracking/Permutations.java
@@ -1,6 +1,7 @@
 package backtracking;
 
 import java.util.ArrayList;
+// import java.util.HashSet;
 
 public class Permutations {
     
@@ -36,4 +37,37 @@ public class Permutations {
 
         }
     }
+
+
+    // below method using hashset
+    // TC: O(N * N!)
+    // SC: O(N)
+    // public void backtrack(ArrayList<Integer> A, ArrayList<ArrayList<Integer>> result, ArrayList<Integer> r, HashSet<Integer> set)
+    // {
+    //     if(r.size() == A.size())
+    //     {
+    //         result.add(new ArrayList<>(r));
+    //         return ;
+    //     }
+        
+    //     for(int j = 0; j< A.size();j++)
+    //     {
+    //         if(!set.contains(A.get(j)))
+    //         {
+    //             r.add(A.get(j));
+    //             set.add(A.get(j));
+    //             backtrack(A, result, r, set);  
+    //             r.remove(r.size() - 1);
+    //             set.remove(A.get(j));
+    //         }
+    //     }
+    // }
+    // public ArrayList<ArrayList<Integer>> permute(ArrayList<Integer> A) {
+    //     ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+    //     ArrayList<Integer> r = new ArrayList<>();
+    //     HashSet<Integer> set = new HashSet<>();
+    //     backtrack(A, result, r, set);
+        
+    //     return result;
+    // }
 }
