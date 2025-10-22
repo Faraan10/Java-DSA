b7faa9682b46a228311969156b92b5fce010adce
Muhammed <faraanshaik19@gmail.com>
2025-10-22 17:30:43 +0530

    commit

--- backtracking/Permutations.java
+++ backtracking/Permutations.java
@@ -7,33 +7,33 @@ public class Permutations {
     // TC: O(N* N!)
     // SC: O(N)
     public ArrayList<ArrayList<Integer>> permute(ArrayList<Integer> A) {
-        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-        boolean[] used = new boolean[A.size()]; // Track which elements are used
-        backtrack(A, new ArrayList<>(), used, result);
-        return result;
+        
+        ArrayList<ArrayList<Integer>> ans=new ArrayList<ArrayList<Integer>>();
+
+        backtrack(ans, A, 0, A.size());
+
+        return ans;
     }
 
-    public static void backtrack(ArrayList<Integer> A, ArrayList<Integer> current, boolean[] used, ArrayList<ArrayList<Integer>> result) {
-        // Base case - when we have a complete permutation
-        if (current.size() == A.size()) {
-            result.add(new ArrayList<>(current)); // Add a copy of the current permutation
+    public static void backtrack(ArrayList<ArrayList<Integer>> ans, ArrayList<Integer> A, int idx, int N){
+
+        if(idx==N){
+            ans.add(new ArrayList<>(A));
             return;
         }
 
-        // Try placing each number in the current permutation
-        for (int i = 0; i < A.size(); i++) {
-            if (used[i]) continue; // Skip already used numbers
+        for(int i=idx; i<A.size(); i++){
 
-            // Choose
-            used[i] = true;
-            current.add(A.get(i));
+            int temp=A.get(idx);
+            A.set(idx, A.get(i));
+            A.set(i, temp);
 
-            // Explore
-            backtrack(A, current, used, result);
+            backtrack(ans, A, idx+1, N);
 
-            // Un-choose (backtrack)
-            current.remove(current.size() - 1);
-            used[i] = false;
+            temp=A.get(idx);
+            A.set(idx, A.get(i));
+            A.set(i, temp);
+
         }
     }
 }
