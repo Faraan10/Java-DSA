aa9916c7cbf6dfeb5cccd8513d4b3600daf6917e
Muhammed <faraanshaik19@gmail.com>
2025-10-28 23:58:47 +0530

    commit

--- trees/binaryTrees/NextPointerBinaryTree.java
+++ trees/binaryTrees/NextPointerBinaryTree.java
@@ -18,22 +18,24 @@ class TreeLinkNode{
 
 public class NextPointerBinaryTree {
     
+    // TC: O(N) travelling through each node once 
+    // SC: O(N) using queue for storing nodes
     public void solveNextPointer(TreeLinkNode root){
 
         // here below I a using BFS approach using queue
         // as Queue is an Interface we have to to either LinkedList or Arraydeque
 
         Queue<TreeLinkNode> que=new LinkedList<>();
-
         que.add(root);
-        TreeLinkNode prev=null;
         
         while(!que.isEmpty()){
 
             int size=que.size();
-            TreeLinkNode curr=que.remove();
+            TreeLinkNode prev=null;
 
             for(int i=0; i<size; i++){
+                
+                TreeLinkNode curr=que.remove();
 
                 if(prev != null){
                     prev.next=curr;
@@ -43,8 +45,8 @@ public class NextPointerBinaryTree {
                 if(curr.left != null) que.add(curr.left);
                 if(curr.right != null) que.add(curr.right);
 
-                prev.next=null;
             }
+            prev.next=null;
         }
     }
 }
