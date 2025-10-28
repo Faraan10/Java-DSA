6a914907e3fb7214f3f779280df0dcc5fb704d5b
Muhammed <faraanshaik19@gmail.com>
2025-10-28 23:50:53 +0530

    commit

---
+++ trees/binaryTrees/NextPointerBinaryTree.java
@@ -0,0 +1,21 @@
+package trees.binaryTrees;
+
+class TreeLinkNode{
+
+    int val;
+    TreeLinkNode left;
+    TreeLinkNode right;
+    TreeLinkNode next;
+
+    TreeLinkNode(int x){
+        val=x;
+        left=null;
+        right=null;
+        next=null;
+    }
+}
+
+public class NextPointerBinaryTree {
+    
+    
+}
