6dc5ffca37f8ff304815ccc7ff8bae7ff7f58944
Muhammed <faraanshaik19@gmail.com>
2025-11-01 23:44:43 +0530

    commit

---
+++ twoPointersApproach/RescueBoats.java
@@ -0,0 +1,27 @@
+package twoPointersApproach;
+import java.util.*;
+
+public class RescueBoats {
+    
+    public int solve(int[] A, int B){
+
+        int count=0;
+
+        Arrays.sort(A);
+
+        int i=0;
+        int j=A.length-1;
+
+        while(i<=j){
+            if(A[i]+A[j]<=B){
+                i++;
+                j--;
+            }else{
+                j--;
+            }
+            count++;
+        }
+
+        return count;
+    }
+}
