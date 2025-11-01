33b07016977aa3c52754b5437adbfe6607b64c93
Muhammed <faraanshaik19@gmail.com>
2025-11-02 00:08:12 +0530

    tc and sc commit

--- linkedList/singleLinkedList/RemoveDuplicatesInSortedList.java
+++ linkedList/singleLinkedList/RemoveDuplicatesInSortedList.java
@@ -2,6 +2,8 @@ package linkedList.singleLinkedList;
 
 public class RemoveDuplicatesInSortedList {
     
+    // TC: O(N)
+    // SC: O(1)
     public ListNode deleteDuplicates(ListNode A) {
 
         if(A==null){
