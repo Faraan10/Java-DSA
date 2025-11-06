10b4f9d4212e3c9113abaacb85e0e728780797b3
Muhammed <faraanshaik19@gmail.com>
2025-11-07 02:24:04 +0530

    commit

--- arrays/GoodPair.java
+++ arrays/GoodPair.java
@@ -5,17 +5,19 @@ public class GoodPair {
     // TC: O(N^2)
     // SC: O(1)
     // bruteforce
-    public int solve(int[] A, int B){
+    // will work for i!=j as j starting from i+1 so i will not be equal to j 
+    // at any point
+    // public int solve(int[] A, int B){
 
-        int N=A.length;
+    //     int N=A.length;
 
-        for(int i=0; i<N; i++){
-            for(int j=i+1; j<N; j++){
-                if(A[i]+A[j]==B){
-                    return 1;
-                }
-            }
-        }
-        return 0;
-    }
+    //     for(int i=0; i<N; i++){
+    //         for(int j=i+1; j<N; j++){
+    //             if(A[i]+A[j]==B){
+    //                 return 1;
+    //             }
+    //         }
+    //     }
+    //     return 0;
+    // }
 }
