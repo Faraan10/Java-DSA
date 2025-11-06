13bbf197e37758cf692151a098d9b213273a3b68
Muhammed <faraanshaik19@gmail.com>
2025-11-06 23:46:16 +0530

    problem commit

--- hashing/FrequencyOfElementQuery.java
+++ hashing/FrequencyOfElementQuery.java
@@ -1,6 +1,29 @@
 package hashing;
 
+import java.util.HashMap;
+
 public class FrequencyOfElementQuery {
     
-    
+    public int[] solve(int[] A, int[] B) {
+
+        HashMap<Integer, Integer> hm=new HashMap<>();
+        int N=A.length;
+
+        for(int i=0; i<N; i++){
+            if(hm.containsKey(A[i])){
+                hm.put(A[i], hm.getOrDefault(A[i], 0)+1);
+            }else{
+                hm.put(A[i], 1);
+            }
+        }
+
+        int M=B.length;
+        int[] arr=new int[M];
+        for(int i=0; i<M; i++){
+            if(hm.containsKey(B[i])){
+                arr[i]=hm.get(B[i]);
+            }
+        }
+        return arr;
+    }
 }
