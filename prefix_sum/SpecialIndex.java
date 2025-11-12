51a0b50ea5e9d3983dda64faa846c717c1d43be8
Muhammed <faraanshaik19@gmail.com>
2025-11-12 23:43:30 +0530

    problem commit

---
+++ prefix_sum/SpecialIndex.java
@@ -0,0 +1,33 @@
+package prefix_sum;
+
+public class SpecialIndex {
+    
+    public int solve(int[] A) {
+
+        int N=A.length;
+        int count=0;
+
+        for(int i=0; i<N; i++){
+            int evenSum=0;
+            int oddSum=0;
+            int index=0;
+
+            for(int j=0; j<N; j++){
+                if(i==j){
+                    continue;
+                }
+
+                if(index%2==0){
+                    evenSum+=A[j];
+                }else{
+                    oddSum+=A[j];
+                }
+                index++;
+            }
+            if(evenSum==oddSum){
+                count++;
+            }
+        }
+        return count;
+    }
+}
