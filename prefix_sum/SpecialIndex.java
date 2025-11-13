da45dd60267218aea27546fdabe33bd9fb1e62e0
Muhammed <faraanshaik19@gmail.com>
2025-11-14 00:47:00 +0530

    optimized problem commit

--- prefix_sum/SpecialIndex.java
+++ prefix_sum/SpecialIndex.java
@@ -33,4 +33,53 @@ public class SpecialIndex {
         }
         return count;
     }
+
+
+    // optimized approach using prefix sum
+    
+    public int optimizedSolve(int[] A) {
+
+        int N=A.length;
+
+        int[] even=new int[N];
+        int[] odd=new int[N];
+        even[0]=A[0];
+        odd[0]=0;
+
+        for(int i=1; i<N; i++){
+            if(i%2==0){
+                even[i]=even[i-1]+A[i];
+            }else{
+                even[i]=even[i-1];
+            }
+            if(i%2==1){
+                odd[i]=odd[i-1]+A[i];
+            }else{
+                odd[i]=odd[i-1];
+            }
+        }
+
+        int count=0;
+        int totalEven=even[N-1];
+        int totalOdd=odd[N-1];
+        for(int i=0; i<N; i++){
+            int leftEven;
+            int leftOdd;
+            if(i==0){
+                leftEven=0;
+                leftOdd=0;
+            }else{
+                leftEven=even[i-1];
+                leftOdd=odd[i-1];
+            }
+
+            int rightEven=totalOdd-odd[i];
+            int rightOdd=totalEven-even[i];
+
+            if(leftEven+rightEven == leftOdd+rightOdd){
+                count++;
+            }
+        }
+        return count;
+    }
 }
