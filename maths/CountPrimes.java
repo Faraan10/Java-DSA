f55b68d83b9795e58be439dada9625c394dde1d8
Muhammed <faraanshaik19@gmail.com>
2025-11-16 23:52:50 +0530

    problem commit

--- maths/CountPrimes.java
+++ maths/CountPrimes.java
@@ -1,5 +1,7 @@
 package maths;
 
+import java.util.Arrays;
+
 public class CountPrimes {
     
     // TC: O(N root N)
@@ -26,6 +28,37 @@ public class CountPrimes {
         for(int i=1; i<=A; i++){
             if(primeNumber(i)==true){
             count++;
+            }
+        }
+        return count;
+    }
+
+
+    // most optimized approach
+    // Sieve of Eratosthenes
+    public int optimizedSolve(int A) {
+
+        if(A<=1){
+            return 0;
+        }
+
+        boolean[] isPrime=new boolean[A+1];
+
+        Arrays.fill(isPrime, true);
+        isPrime[0]=false;
+        isPrime[1]=false;
+
+        for(int i=2; i*i<=A; i++){
+            for(int j=i*i; j<=A; j=j+i){
+                isPrime[j]=false;
+            }
+        }
+
+        int count=0;
+
+        for(int i=2; i<=A; i++){
+            if(isPrime[i]){
+                count++;
             }
         }
         return count;
