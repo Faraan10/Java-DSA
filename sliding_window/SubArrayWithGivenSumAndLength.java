ddcdeb9e5c99d66b1b7daf72744a6d9c792840dd
Muhammed <faraanshaik19@gmail.com>
2025-11-17 00:38:20 +0530

    problem commit

---
+++ sliding_window/SubArrayWithGivenSumAndLength.java
@@ -0,0 +1,28 @@
+package sliding_window;
+
+public class SubArrayWithGivenSumAndLength {
+    
+    public int solve(int[] A, int B, int C) {
+
+        int N=A.length;
+        int sum=0;
+        for(int i=0; i<B; i++){
+            sum+=A[i];
+        }
+        if(sum==C){
+            return 1;
+        }
+
+        int i=1;
+        int j=B;
+        while(j<N){
+            sum=sum-A[i-1]+A[j];
+            if(sum==C){
+                return 1;
+            }
+            i++;
+            j++;
+        }
+        return 0;
+    }
+}
