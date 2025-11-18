116b05f3fa243e11af4fa0706e1f1db5cbc6bc67
Muhammed <faraanshaik19@gmail.com>
2025-11-19 01:05:07 +0530

    commit

--- arrays/subarrays/CountingSubArraySum.java
+++ arrays/subarrays/CountingSubArraySum.java
@@ -21,4 +21,36 @@ public class CountingSubArraySum {
         }
         return count;
     }
+
+
+    // problem statement:
+    // Given an array A of N non-negative numbers and a non-negative number B,
+    // you need to find the number of subarrays in A with a sum less than B.
+    // We may assume that there is no overflow.
+
+    // by the problem statement it says that there are only non-negative integers
+    // so when adding sum it will keep on increasing without change (mean decreasing)
+    // which is monotonic condition so we can use sliding window
+    // most optmized solution using (Sliding window)
+    // TC: O(N)
+    // SC: O(1)
+    public int optimizedSolve(int[] A, int B) {
+
+        int N=A.length;
+        int left=0;
+        int sum=0;
+        int count=0;
+
+        for(int right=0; right<N; right++){
+            sum+=A[right];
+
+            while(sum>=B && left<=right){
+                sum=sum-A[left];
+                left++;
+            }
+
+            count=count+(right-left+1);
+        }
+        return count;
+    }
 }
