0cfa6f44090187dbd46b39e288cfd1d70c517671
Muhammed <faraanshaik19@gmail.com>
2025-11-20 00:18:39 +0530

    problem commit

--- arrays/sorting/ElementsRemoval.java
+++ arrays/sorting/ElementsRemoval.java
@@ -26,4 +26,47 @@ public class ElementsRemoval {
        }        
        return cost;
     }
+
+
+    // using prefix sum
+    // TC: O(N log N)
+    // SC: O(N)
+
+    public int prefixSolve(int[] A) {
+
+       int N=A.length;
+       Arrays.sort(A);
+
+       int[] pf=new int[N];
+       pf[0]=A[0];
+       for(int i=1; i<N; i++){
+           pf[i]=pf[i-1]+A[i];
+       }
+
+       int sum=0;
+       for(int i=0; i<N; i++){
+           sum+=pf[i];
+       }
+       return sum;
+    }
+
+
+
+    // TC: O(N)
+    // SC: O(1)
+    // using the index ( (N-i) represents the no of times element in counted 
+    // in array *A[i]) and add it to sum
+
+    public int optimizedSolve(int[] A) {
+
+        int N=A.length;
+        Arrays.sort(A);
+        int ans=0;
+
+        for(int i=N-1; i>=0; i--){
+            ans=ans+((N-i)*A[i]);
+        }
+        return ans;
+    }
+
 }
