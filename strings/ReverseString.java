38479db3711ddc10272f974efea50416ee073139
Muhammed <faraanshaik19@gmail.com>
2025-11-21 01:03:26 +0530

    problem commit

---
+++ strings/ReverseString.java
@@ -0,0 +1,23 @@
+package strings;
+
+public class ReverseString {
+    
+    // TC: O(N)
+    // SC: O(N)
+    public String solve(String A) {
+
+        char[] arr=A.toCharArray();
+
+        int start=0;
+        int end=arr.length-1;
+
+        while(start<end){
+            char temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+        return new String(arr);
+    }
+}
