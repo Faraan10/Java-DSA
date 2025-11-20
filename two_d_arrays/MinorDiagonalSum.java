035e5bb4cf6b07f885e57765d604202ace3463f4
Muhammed <faraanshaik19@gmail.com>
2025-11-21 00:49:03 +0530

    problem commit

---
+++ two_d_arrays/MinorDiagonalSum.java
@@ -0,0 +1,23 @@
+package two_d_arrays;
+
+public class MinorDiagonalSum {
+    
+    // TC: O(N)
+    // SC: O(1)
+    public int solve(final int[][] A) {
+       
+       int N=A.length;
+       int M=A[0].length;
+
+       int i=0;
+       int j=M-1;
+       int sum=0;
+
+       while(i<N && j>=0){
+           sum+=A[i][j];
+           i++;
+           j--;
+       }
+       return sum;
+    }
+}
