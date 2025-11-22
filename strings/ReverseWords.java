ff9068e701d2303aa4b672f9760446afe2184d06
Muhammed <faraanshaik19@gmail.com>
2025-11-22 18:25:06 +0530

    optimized problem commit

--- strings/ReverseWords.java
+++ strings/ReverseWords.java
@@ -51,4 +51,42 @@ public class ReverseWords {
         }
         return sb.toString();
     }
+
+
+
+    // optimized string builder ( without additional string builder and without reverse method)
+    // TC: O(N) 
+    // SC: O(1) ( only stringBuilder for creating new string ) which is the output
+
+    public String optimizedSolve(String A) {
+
+        int N=A.length();        
+        StringBuilder sb=new StringBuilder();
+
+        for(int i=N-1; i>=0; i--){
+
+            while(i>=0 && A.charAt(i)==' '){
+                i--;
+            }
+            if(i<0){
+                break;
+            }
+
+            int end=i;
+
+            while(i>=0 && A.charAt(i)!=' '){
+                i--;
+            }
+            
+            int start=i+1;
+
+            if(sb.length()>0){
+                sb.append(' ');
+            }
+
+
+            sb.append(A.substring(start, end+1));
+        }
+        return sb.toString();
+    }
 }
