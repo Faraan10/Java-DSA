bf10504ebd49a06a3a19cfc17a01cfe267760a98
Muhammed <faraanshaik19@gmail.com>
2025-11-27 00:20:16 +0530

    commit

--- arrays/kadenesAlgorithm.java
+++ arrays/kadenesAlgorithm.java
@@ -18,23 +18,6 @@ public class kadenesAlgorithm {
         return ans;
     }
 
-    // TC: O(N)
-    // SC: O(1)
-    public int maxSubArraySum(final int[] A) {
-
-        int N=A.length;
-        int ans=Integer.MIN_VALUE;
-        int sum=0;
-        for(int i=0; i<N; i++){
-            sum+=A[i];
-            ans=Math.max(ans, sum);
-            if(sum<0){
-                sum=0;
-            }
-        }
-        return ans;
-    }
-
 
     // in the below approach I am also including start and end 
     // indices of that subarray
