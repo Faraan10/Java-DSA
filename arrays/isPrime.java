162b413d1f350f2a9289fc595837c0bbfd49a979
Muhammed <faraanshaik19@gmail.com>
2025-12-04 22:39:36 +0530

    commit

--- arrays/isPrime.java
+++ arrays/isPrime.java
@@ -2,7 +2,14 @@ package arrays;
 
 public class isPrime {
     
-    // TC: O(root A)
+    // TC: O(root A) 
+    // suppose N=100 i will only go till 10
+    // 10*10 =100
+    // 1 and 100 are factors - so here 2 factors
+    // 2 and 50 are factors - so here 2 factors
+    // 4 and 25 are factors - so here 2 factors
+    // 5 and 20 are factors - so here 2 factors
+    // 10 and 10 are factors - so here 1 factor as 10 is repeating on both sides
     // SC: O(1)
     public int solve(int A) {
 
