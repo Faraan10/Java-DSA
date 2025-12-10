9d17db8a537c60eb072390e52b1148ceddc14b86
Muhammed <faraanshaik19@gmail.com>
2025-12-11 01:20:56 +0530

    problem commit

--- two_d_arrays/advance/SearchInRowWiseAndColumnWiseSortedMatrix.java
+++ two_d_arrays/advance/SearchInRowWiseAndColumnWiseSortedMatrix.java
@@ -2,6 +2,10 @@ package two_d_arrays.advance;
 
 public class SearchInRowWiseAndColumnWiseSortedMatrix {
     
+    // TC: O(N+M)
+    // because we are moving atmost N steps downwards and atmost M steps 
+    // sideways in this case to left as we are starting from top right position
+    // SC: O(1)
     public int solve(int[][] A, int B) {
 
         int N=A.length;
