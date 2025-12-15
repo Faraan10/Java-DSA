286ae9cc32ac9681d015af4a303939dc026b0d1a
Muhammed <faraanshaik19@gmail.com>
2025-12-15 23:37:48 +0530

    tc and sc commit

--- arrays/SecondLargest.java
+++ arrays/SecondLargest.java
@@ -1,6 +1,26 @@
 package arrays;
 
 public class SecondLargest {
+
+    // TC: O(N)
+    // SC: O(1)
+    public int twoLoopsSolve(int[] A) {
+
+        int N=A.length;
+        int largest=A[0];
+        int secondLargest=-1;
+
+        for(int i=1; i<N; i++){
+            largest=Math.max(A[i], largest);
+        }
+
+        for(int i=0; i<N; i++){
+            if(A[i] != largest){
+                secondLargest=Math.max(secondLargest, A[i]);
+            }
+        }
+        return secondLargest;
+    }
     
     // TC: O(N)
     // SC: O(1)
