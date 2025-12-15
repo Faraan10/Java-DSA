e1aec90636cc85f3ea3ace2ec899361e3d9b2474
Muhammed <faraanshaik19@gmail.com>
2025-12-15 23:36:54 +0530

    problem commit

--- arrays/SecondLargest.java
+++ arrays/SecondLargest.java
@@ -2,4 +2,21 @@ package arrays;
 
 public class SecondLargest {
     
+    
+    public int solve(int[] A) {
+
+        int N=A.length;
+        int largest=A[0];
+        int secondLargest=-1;
+
+        for(int i=1; i<N; i++){
+            if(A[i]>largest){
+                secondLargest=largest;
+                largest=A[i];
+            }else if(A[i]<largest && A[i]>secondLargest){
+                secondLargest=A[i];
+            }
+        }
+        return secondLargest;
+    }
 }
