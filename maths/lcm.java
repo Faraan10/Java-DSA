718ab1281f339ee818991450eeb6e25a254fa457
Muhammed <faraanshaik19@gmail.com>
2025-12-24 02:38:42 +0530

    problem commit

---
+++ maths/lcm.java
@@ -0,0 +1,28 @@
+package maths;
+
+import java.util.Scanner;
+
+public class lcm {
+    
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        
+        
+        Scanner sc=new Scanner(System.in);
+
+        int A=sc.nextInt();
+        int B=sc.nextInt();
+
+        int val=Math.max(A,B);
+
+        for(int i=val; i<=A*B; i++){
+            if(i%A==0 && i%B==0){
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
