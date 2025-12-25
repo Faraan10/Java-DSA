842664fc49eb39279acd505558ae81a824c08a57
Muhammed <faraanshaik19@gmail.com>
2025-12-25 18:28:50 +0530

    problem commit

--- maths/PalindromicInteger.java
+++ maths/PalindromicInteger.java
@@ -4,26 +4,48 @@ import java.util.Scanner;
 
 public class PalindromicInteger {
 
+    
+
+    // TC: O(N)
+    // SC: O(1)
+
     public static void main(String[] args) {
     
         Scanner sc=new Scanner(System.in);
         
+        // TC :O(log A)
+        // SC: O(1)
         int N=sc.nextInt();
-        boolean isPalin=true;
-        String str=Integer.toString(N);
 
-        for(int i=0; i<str.length()/2; i++){
-            if((str.charAt(i) != (str.charAt(str.length()-1-i)))){
-                isPalin=false;
-                break;
-            }else{
-            }
+        int A=N;
+        int remainder=0;
+        while(A!=0){
+            int digit=A%10;
+            remainder=remainder*10+digit;
+            A=A/10;
         }
-        if(isPalin){
+        if(remainder==N){
             System.out.println("Yes");
         }else{
             System.out.println("No");
         }
+
+
+        // boolean isPalin=true;
+        // String str=Integer.toString(N);
+
+        // for(int i=0; i<str.length()/2; i++){
+        //     if((str.charAt(i) != (str.charAt(str.length()-1-i)))){
+        //         isPalin=false;
+        //         break;
+        //     }else{
+        //     }
+        // }
+        // if(isPalin){
+        //     System.out.println("Yes");
+        // }else{
+        //     System.out.println("No");
+        // }
         sc.close();
     }
 
