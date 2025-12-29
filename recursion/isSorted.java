39d163b6bb57b1d579195fef6d8d1b211f458cb4
Muhammed <faraanshaik19@gmail.com>
2025-12-29 22:01:15 +0530

    problem commit

--- recursion/isSorted.java
+++ recursion/isSorted.java
@@ -2,4 +2,16 @@ package recursion;
 
 public class isSorted {
     
+    public boolean isItSorted(int[] A, int idx) {
+
+        if(idx==A.length-1){
+            return true;
+        }
+
+        if(A[idx]>A[idx+1]){
+            return false;
+        }
+
+        return isItSorted(A, idx+1);
+    } 
 }
