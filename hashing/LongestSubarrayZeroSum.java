944c05444aaa24376e2b99b9ca3db296cdef7120
Muhammed <faraanshaik19@gmail.com>
2026-01-02 19:29:39 +0530

    problem commit

--- hashing/LongestSubarrayZeroSum.java
+++ hashing/LongestSubarrayZeroSum.java
@@ -1,5 +1,30 @@
 package hashing;
 
+import java.util.HashMap;
+
 public class LongestSubarrayZeroSum {
-    
+    public int solve(int[] A) {
+
+         int N = A.length;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        int sum = 0;
+        int maxLen = 0;
+
+        // prefix sum 0 occurs before array starts
+        hm.put(0, -1);
+
+        for (int i = 0; i < N; i++) {
+            sum += A[i];
+
+            if (hm.containsKey(sum)) {
+                int prevIndex = hm.get(sum);
+                maxLen = Math.max(maxLen, i - prevIndex);
+            } else {
+                // store first occurrence only
+                hm.put(sum, i);
+            }
+        }
+        return maxLen;
+    }
 }
