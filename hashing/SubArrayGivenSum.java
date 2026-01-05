53b2a1ae944e75ec65f4882e580b491046e6cd86
Muhammed <faraanshaik19@gmail.com>
2026-01-05 23:00:41 +0530

    commit

--- hashing/SubArrayGivenSum.java
+++ hashing/SubArrayGivenSum.java
@@ -7,12 +7,12 @@ public class SubArrayGivenSum {
     public int[] solve(int[] A, int B) {
 
         int N=A.length;
+
         int left=0;
         int sum=0;
         int start=-1;
         int end=-1;
         boolean isFound=false;
-
         for(int right=0; right<N; right++){
             sum+=A[right];
             while(sum>B){
@@ -26,15 +26,16 @@ public class SubArrayGivenSum {
                 break;
             }
         }
-        if((start==-1 && end==-1) || isFound==false){
-            int[] a=new int[1];
-            a[0]=-1;
-            return a;
+        if(!isFound){
+            int[] arr=new int[1];
+            arr[0]=-1;
+            return arr;
         }
-        int[] arr=new int[end-start+1];
 
-        for(int i=0; i<arr.length; i++){
-            arr[i]=A[start++];
+        int[] arr=new int[end-start+1];
+        int count=0;
+        for(int i=start; i<=end; i++){
+            arr[count++]=A[i];
         }
         return arr;
     }
