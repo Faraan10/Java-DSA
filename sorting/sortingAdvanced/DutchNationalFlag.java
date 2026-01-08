b286478cea361045fef1720f65da161e0299ac4c
Muhammed <faraanshaik19@gmail.com>
2026-01-08 23:43:39 +0530

    problem commit

---
+++ sorting/sortingAdvanced/DutchNationalFlag.java
@@ -0,0 +1,40 @@
+package sorting.sortingAdvanced;
+
+public class DutchNationalFlag {
+    
+    public int[] sortColors(int[] A) {
+
+        int N=A.length;
+
+        int[] arr=new int[N];
+
+        int red=0;
+        int white=0;
+        int blue=0;
+        for(int i=0; i<N; i++){
+            if(A[i]==0){
+                red++;
+            }else if(A[i]==1){
+                white++;
+            }else{
+                blue++;
+            }
+        }
+
+        int index=0;
+        for(int i=0; i<red; i++){
+            arr[i]=0;
+            index++;
+        }
+        
+        for(int i=0; i<white; i++){
+            arr[index]=1;
+            index++;
+        }
+        for(int i=0; i<blue; i++){
+            arr[index]=2;
+            index++;
+        }
+        return arr;
+    } 
+}
