3e54293847c52647bdf37efc87b55c8b696a8903
Muhammed <faraanshaik19@gmail.com>
2026-01-12 23:36:26 +0530

    problem commit

--- sorting/PartitionIndex.java
+++ sorting/PartitionIndex.java
@@ -7,24 +7,24 @@ public class PartitionIndex {
     public int partition(int[] arr){
 
         int N=arr.length;
-        int i=0;
-        int j=0;
-        int pivot=arr[N-1];
+        int i=-1; // this is the index partition point where all the elements
+        // till here are <=pivot and after here will be greater than pivot
+        int pivot=arr[N-1]; // this is the pivot element we do not alter this 
+        // till final step and do checks just before it
 
-        while(i<N-1){
-            if(arr[i]<=pivot){
+        for(int j=0; j<N-1; j++){
+            if(arr[j]<=pivot){
+                i++;
                 int temp=arr[i];
                 arr[i]=arr[j];
                 arr[j]=temp;
-                j++;
             }
-            i++;
         }
+        int temp=arr[i+1];
+        arr[i+1]=arr[N-1];
+        arr[N-1]=temp;
 
-        int temp=arr[N-1];
-        arr[N-1]=arr[j];
-        arr[j]=temp;
-
-        return j;
+        return i+1; // this is the partition index where before are element<= 
+        // and after index elements > greater
     }
 }
