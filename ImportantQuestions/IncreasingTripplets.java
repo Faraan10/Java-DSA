01565e3263a43a42b810339e868d85ab21abc83b
Muhammed <faraanshaik19@gmail.com>
2026-01-16 23:51:09 +0530

    problem commit

---
+++ ImportantQuestions/IncreasingTripplets.java
@@ -0,0 +1,23 @@
+package ImportantQuestions;
+
+public class IncreasingTripplets {
+    
+    public int solve(int[] A) {
+        int n = A.length , ans = 0;
+        for(int i = 0 ; i < n ; i++){
+            int l = 0 , r = 0;
+            for(int j = 0 ; j < i ; j++){
+                if(A[j] < A[i]){
+                    l += 1;
+                }
+            }
+            for(int j = i + 1 ; j < n ; j++){
+                if(A[i] < A[j]){
+                    r += 1;
+                }
+            }
+            ans += (l * r);
+        }
+        return ans;
+    }
+}
