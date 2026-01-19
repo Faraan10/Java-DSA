07b9a1d254033c924efaee1221404c550d6ab23f
Muhammed <faraanshaik19@gmail.com>
2026-01-19 23:39:30 +0530

    commit

--- stacks/NearestSmallerElement.java
+++ stacks/NearestSmallerElement.java
@@ -8,20 +8,20 @@ public class NearestSmallerElement {
     // SC: O(N)
     public int[] prevSmaller(int[] A) {
 
-    int[] arr = new int[A.length];
-    Stack<Integer> st = new Stack<>();
+        int[] arr = new int[A.length];
+        Stack<Integer> st = new Stack<>();
 
-    for (int i = 0; i < A.length; i++) {
-        while (!st.isEmpty() && st.peek() >= A[i]) {
-            st.pop();
+        for (int i = 0; i < A.length; i++) {
+            while (!st.isEmpty() && st.peek() >= A[i]) {
+                st.pop();
+            }
+            if (st.isEmpty()) {
+                arr[i] = -1;
+            } else {
+                arr[i] = st.peek();
+            }
+            st.push(A[i]);
         }
-        if (st.isEmpty()) {
-            arr[i] = -1;
-        } else {
-            arr[i] = st.peek();
-        }
-        st.push(A[i]);
-    }
-    return arr;
+        return arr;
     }
 }
