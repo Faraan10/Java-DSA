c5458f2036164e7d4d6f9ba54693661bc5915c81
Muhammed <faraanshaik19@gmail.com>
2026-01-21 00:37:49 +0530

    problem commit

--- BinarySearch/AggresiveCows.java
+++ BinarySearch/AggresiveCows.java
@@ -45,4 +45,51 @@ public class AggresiveCows {
         }
         return ans;
     }
+
+
+    // same approach but with better explanation
+    public boolean isPossibleToPlaceCows(int[] A, int min_length_to_place_cows, int cows_limit){
+
+        int N=A.length;
+        int count=1; // as placing 1st cow at first position
+        int lastIndex=0; // for tracking previous cows position
+
+        for(int i=1; i<N; i++){
+            if(A[i]-A[lastIndex]>=min_length_to_place_cows){
+                count++;
+                lastIndex=i;
+            }
+            if(count>=cows_limit){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public int explainedSolve(int[] A, int B) {
+
+        int N=A.length;
+        // The array has to be sorted for this problem as we are comparing
+        // every element stall wise side by side for distance so,
+
+        Arrays.sort(A);
+        int low=1; // this is the lowest dictance between any two cows
+        int high=A[N-1]-A[0]; // this is the max distance btw any two cows
+        // above both variables are required for search space which is required
+        // for binary search
+        int ans=-1;
+
+        while(low<=high){
+
+            int mid=low+(high-low)/2;
+            
+            if(isPossibleToPlaceCows(A, mid, B)){
+                ans=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
 }
