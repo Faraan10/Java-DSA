db3e20f882b7a875d9eeccc0fcea4852de6b8335
Muhammed <faraanshaik19@gmail.com>
2026-01-23 02:02:13 +0530

    optimized problem commit

--- queues/ParkingIcecreamTruck.java
+++ queues/ParkingIcecreamTruck.java
@@ -39,4 +39,46 @@ public class ParkingIcecreamTruck {
         }
         return arr;
     }
+
+
+    // another good way solving this problem same but using while loop
+    // below first inital loop to make it more easy to understand
+    public int[] differentApproachSlidingMaximum(final int[] A, int B) {
+
+        int N=A.length;
+        Deque<Integer> dq=new LinkedList<>();
+        int[] arr=new int[N-B+1]; // this will be length of the array
+
+        for(int i=0; i<B; i++){
+
+            while(!dq.isEmpty() && A[dq.getLast()]<=A[i]){
+                dq.removeLast();
+            }
+            dq.addLast(i);
+        }
+
+        int i=1;
+        int j=B;
+        int idx=0;
+        while(j<N){
+
+            arr[idx]=A[dq.getFirst()];
+            idx++;
+
+            if(!dq.isEmpty() && dq.getFirst()<i){
+                dq.removeFirst();
+            }
+
+            while(!dq.isEmpty() && A[dq.getLast()]<=A[j]){
+                dq.removeLast();
+            }
+            dq.addLast(j);
+
+            i++;
+            j++;
+        }
+        arr[idx]=A[dq.getFirst()];
+
+        return arr;
+    }
 }
