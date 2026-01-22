8cafbaa12c0127d3dff7e7be580f169e862b30ef
Muhammed <faraanshaik19@gmail.com>
2026-01-23 00:47:27 +0530

    brute force using 2 stacks commit

--- queues/QueueUsingStack.java
+++ queues/QueueUsingStack.java
@@ -3,6 +3,64 @@ package queues;
 import java.util.Stack;
 
 public class QueueUsingStack {
+
+
+
+    // public static class UserQueue {
+    // /** Initialize your data structure here. */
+
+    //     static Stack<Integer> st1;
+    //     static Stack<Integer> st2;
+    //     UserQueue() {
+    //         st1=new Stack<>();
+    //         st2=new Stack<>();
+    //     }
+        
+    //     /** Push element X to the back of queue. */
+    //     static void push(int X) {
+
+    //         st1.push(X);
+    //     }
+        
+    //     /** Removes the element from in front of queue and returns that element. */
+    //     static int pop() {
+            
+    //         while(!st1.isEmpty()){
+    //             st2.push(st1.pop());
+    //         }
+
+    //         int ans=st2.pop();
+
+    //         while(!st2.isEmpty()){
+    //             st1.push(st2.pop());
+    //         }
+
+    //         return ans;
+    //     }
+        
+    //     /** Get the front element of the queue. */
+    //     static int peek() {
+            
+    //         while(!st1.isEmpty()){
+    //             st2.push(st1.pop());
+    //         }
+
+    //         int ans=st2.peek();
+
+    //         while(!st2.isEmpty()){
+    //             st1.push(st2.pop());
+    //         }
+    //         return ans;
+    //     }
+        
+    //     /** Returns whether the queue is empty. */
+    //     static boolean empty() {
+            
+    //         return st1.isEmpty();
+    //     }
+    // }
+
+
     // TC: push: O(1) pop: amortized O(1)  peek: amortized O(1)
     // SC: O(N) stack used
     public static class UserQueue {
