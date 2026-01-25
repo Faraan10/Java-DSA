96f94e385bbe73473b11f99f4c85fa90600c2ed2
Muhammed <faraanshaik19@gmail.com>
2026-01-25 17:41:27 +0530

    problem commit

---
+++ maths/FibonacciNumber.java
@@ -0,0 +1,26 @@
+package maths;
+
+public class FibonacciNumber {
+    
+    // Fibonacci problem iterative approach with array
+    // TC: O(N)
+    // SC: O(1)
+    public int Fibonacci(int N){
+
+        if(N==0){
+            return 0;
+        }
+        if(N==1){
+            return 1;
+        }
+
+        int[] arr=new int[N+1];
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2; i<=N; i++){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+
+        return arr[N];
+    }
+}
