3c9c630028b137bfc6320a28455ace42e00ff707
Muhammed <faraanshaik19@gmail.com>
2026-01-28 02:37:58 +0530

    problem commit left view recursive approach

---
+++ trees/binaryTrees/LeftViewBinaryTree.java
@@ -0,0 +1,29 @@
+package trees.binaryTrees;
+
+import java.util.ArrayList;
+
+public class LeftViewBinaryTree {
+    
+    public ArrayList<Integer> solve(TreeNode A) {
+
+        ArrayList<Integer> list=new ArrayList<>();
+
+        leftView(list, A, 0); // here 0 is the current depth level 
+                                // for tallying with arraylist size
+        return list;
+    }
+
+    public void leftView(ArrayList<Integer> list, TreeNode root, int currentDepth){
+
+        if(root==null){
+            return;
+        }
+
+        if(currentDepth==list.size()){
+            list.add(root.val);
+        }
+
+        leftView(list, root.left, currentDepth+1);
+        leftView(list, root.right, currentDepth+1);
+    }
+}
