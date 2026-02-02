980b0278c3d1f14617378b375cac4a5a537a968d
Muhammed <faraanshaik19@gmail.com>
2026-02-02 23:47:35 +0530

    print numbers 1 to N recursive

--- index.java
+++ index.java
@@ -1,103 +1,125 @@
 public class index {
 
-    public boolean palin(String str, int start, int end){
+    // public boolean palin(String str, int start, int end){
 
-        if(start>=end){
-            return true;
-        }
+    //     if(start>=end){
+    //         return true;
+    //     }
 
-        if(str.charAt(start) != str.charAt(end)){
-            return false;
-        }
-        return palin(str, start+1, end-1);
-    }
+    //     if(str.charAt(start) != str.charAt(end)){
+    //         return false;
+    //     }
+    //     return palin(str, start+1, end-1);
+    // }
 
-    public int palindrome(String S){
+    // public int palindrome(String S){
 
-        int N=S.length();
+    //     int N=S.length();
 
-        boolean flag=palin(S, 0, N-1);
+    //     boolean flag=palin(S, 0, N-1);
 
-        if(flag){
-            return 1;
-        }
-        return 0;
-    }
+    //     if(flag){
+    //         return 1;
+    //     }
+    //     return 0;
+    // }
 
-    public int maxEle(int[] A){
-        return 0;
-    }
+    // public int maxEle(int[] A){
+    //     return 0;
+    // }
 
 
-    // 24-01-2025
-    // parameterized way of writing recurion
-    public int sumOfN(int sum, int i){
+    // // 24-01-2025
+    // // parameterized way of writing recurion
+    // public int sumOfN(int sum, int i){
         
-        if(i<1){
-            return sum;
-        }
+    //     if(i<1){
+    //         return sum;
+    //     }
 
-        return sumOfN(sum+i, i-1);
+    //     return sumOfN(sum+i, i-1);
         
-    }
-    public int sum(int N){
+    // }
+    // public int sum(int N){
 
-        return sumOfN(0, N);
-    }
+    //     return sumOfN(0, N);
+    // }
 
-    // functional way of writing recursive code
-    public int sumOfNumbers(int N){ 
+    // // functional way of writing recursive code
+    // public int sumOfNumbers(int N){ 
 
-        if(N==0){
-            return 1;
-        }
-        return N+sumOfNumbers(N-1);
-    }
-    // suppose N=3,
-    // 3 + sumOfNumbers(3-1) -> so 3 + something it does not know yet 
-    // so it waits for that to return and calculates
-    // so now as sumOfNumbers(N-1) it calls the same function but with value 
-    // of N now is N-1 which is 2
-    // 2 + sumOfNumbers(2-1);
-    // Now value of N is 1 
-    // so it hits the base case 1==1 and returns 1 back to sumOfNumbers(2-1) 
-    // which is 1  and adds 2+1=3 and returns it to sumOfNumbers(3-1) value 3
-    // tehn finally it returns 3+3=6 as output
+    //     if(N==0){
+    //         return 1;
+    //     }
+    //     return N+sumOfNumbers(N-1);
+    // }
+    // // suppose N=3,
+    // // 3 + sumOfNumbers(3-1) -> so 3 + something it does not know yet 
+    // // so it waits for that to return and calculates
+    // // so now as sumOfNumbers(N-1) it calls the same function but with value 
+    // // of N now is N-1 which is 2
+    // // 2 + sumOfNumbers(2-1);
+    // // Now value of N is 1 
+    // // so it hits the base case 1==1 and returns 1 back to sumOfNumbers(2-1) 
+    // // which is 1  and adds 2+1=3 and returns it to sumOfNumbers(3-1) value 3
+    // // tehn finally it returns 3+3=6 as output
 
-    public void reverse(int start, int[] arr, int end){
+    // public void reverse(int start, int[] arr, int end){
 
-        if(start>=end){
+    //     if(start>=end){
+    //         return;
+    //     }   
+    //     int temp=arr[start];
+    //     arr[start]=arr[end];
+    //     arr[end]=temp;
+
+    //     reverse(start+1, arr, end-1);
+    // }
+    // public int[] reverseArray(int[] arr){
+
+    //     reverse(0, arr, arr.length-1);
+    //     return arr;
+    // }
+
+
+    // // TC: O(log A)
+    // // SC: O(1)
+    // public int reverseNumber(int A){
+
+    //     int N=A;
+    //     int remainder=0;
+    //     while(N!=0){
+
+    //         int digit=N%10;
+    //         remainder=remainder*10+digit;
+    //         N=N/10;
+    //     }
+
+    //     if(remainder==A){
+    //         return 1;
+    //     }
+    //     return 0;
+    // }
+
+    public void Numbers(int i, int N){
+
+        if(i==N){
+            System.out.println(i);
             return;
-        }   
-        int temp=arr[start];
-        arr[start]=arr[end];
-        arr[end]=temp;
+        }
+        System.out.println(i);
+        Numbers(i+1, N);
 
-        reverse(start+1, arr, end-1);
     }
-    public int[] reverseArray(int[] arr){
-
-        reverse(0, arr, arr.length-1);
-        return arr;
+    public void printNumbers(int N){
+        Numbers(1, N);
     }
 
+    public static void main(String[] args){
 
-    // TC: O(log A)
-    // SC: O(1)
-    public int reverseNumber(int A){
+        index s1=new index();
 
-        int N=A;
-        int remainder=0;
-        while(N!=0){
+        s1.printNumbers(10);
 
-            int digit=N%10;
-            remainder=remainder*10+digit;
-            N=N/10;
-        }
-
-        if(remainder==A){
-            return 1;
-        }
-        return 0;
     }
 }
