3f13afbd89396d3002b29acbd4c2d17cb97be6c0
Muhammed <faraanshaik19@gmail.com>
2026-02-10 23:28:57 +0530

    commit

--- backtracking/GenerateSubsets.java
+++ backtracking/GenerateSubsets.java
@@ -7,25 +7,25 @@ import java.util.ArrayList;
     // with for loop
 public class GenerateSubsets {
     
-    // public ArrayList<ArrayList<Integer>> subsets(ArrayList<Integer> A) {
-    //     // code here
+    public ArrayList<ArrayList<Integer>> subsetsFor(ArrayList<Integer> A) {
+        // code here
 
-    //     ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-    //     backtrack(A, 0, new ArrayList<>(), result);
-    //     return result;
-    // }
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        backtrack(A, 0, new ArrayList<>(), result);
+        return result;
+    }
 
-    // public static void backtrack(ArrayList<Integer> A, int index, ArrayList<Integer> current, ArrayList<ArrayList<Integer>> result) {
-    //     // Add current subset to result
-    //     result.add(new ArrayList<>(current));
+    public static void backtrack(ArrayList<Integer> A, int index, ArrayList<Integer> current, ArrayList<ArrayList<Integer>> result) {
+        // Add current subset to result
+        result.add(new ArrayList<>(current));
 
-    //     // Explore further elements
-    //     for (int i = index; i < A.size(); i++) {
-    //         current.add(A.get(i));              // Choose
-    //         backtrack(A, i + 1, current, result); // Explore
-    //         current.remove(current.size() - 1);   // Unchoose (backtrack)
-    //     }
-    // }
+        // Explore further elements
+        for (int i = index; i < A.size(); i++) {
+            current.add(A.get(i));              // Choose
+            backtrack(A, i + 1, current, result); // Explore
+            current.remove(current.size() - 1);   // Unchoose (backtrack)
+        }
+    }
 
 
     // used backtracking approach here with a backtrack void function below
