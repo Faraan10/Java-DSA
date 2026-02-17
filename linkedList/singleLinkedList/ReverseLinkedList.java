08ca7a07f2551aa205ca488345cb628dc2b6c327
Muhammed <faraanshaik19@gmail.com>
2026-02-17 21:10:15 +0530

    commit

--- linkedList/singleLinkedList/ReverseLinkedList.java
+++ linkedList/singleLinkedList/ReverseLinkedList.java
@@ -15,7 +15,7 @@ public class ReverseLinkedList {
     
     // TC: O(N) travelling through entire linked list once
     // SC: O(1) only 3 variables used as pointers
-    // current, previos and next
+    // current, previous and next
     public ListNode reverseList(ListNode A) {
 
         ListNode current=A;
