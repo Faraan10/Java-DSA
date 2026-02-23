8236f69b38e5cb063e8772e920770e7c3fcda1eb
Muhammed <faraanshaik19@gmail.com>
2026-02-23 23:58:33 +0530

    problem commit

---
+++ revision/SpecialElement.java
@@ -0,0 +1,24 @@
+package revision;
+
+public class SpecialElement {
+    
+    public int specialEle(int[] A){
+
+        int N=A.length;
+        int total=0;
+        for(int i=0; i<N; i++){
+            total+=A[i];
+        }
+
+        int left=0;
+        for(int i=0; i<N; i++){
+
+            int right=total-left-A[i];
+            if(right==left){
+                return 1;
+            }
+            left+=A[i];
+        }
+        return 0;
+    }
+}
