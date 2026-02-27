0c5b6681f39edaa7635533ac269c50d7195ac382
Muhammed <faraanshaik19@gmail.com>
2026-02-27 23:35:52 +0530

    problem commit

--- sorting/KthSmallest.java
+++ sorting/KthSmallest.java
@@ -1,5 +1,7 @@
 package sorting;
 
+import java.util.Arrays;
+
 public class KthSmallest {
     
     // TC: O(N*B)
@@ -22,6 +24,15 @@ public class KthSmallest {
             A[i]=A[idx];
             A[idx]=temp;
         }
+        return A[B-1];
+    }
+
+    // TC: O(N log N)
+    // SC: O(1)
+    public int kthsmallestInBuiltSorting(final int[] A, int B) {
+
+        Arrays.sort(A);
+
         return A[B-1];
     }
 }
