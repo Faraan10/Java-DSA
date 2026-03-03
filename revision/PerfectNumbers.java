2714398c9836ad9b0f1b842bb679bb4d6e01609e
Muhammed <faraanshaik19@gmail.com>
2026-03-03 23:58:21 +0530

    problem commit

--- revision/PerfectNumbers.java
+++ revision/PerfectNumbers.java
@@ -2,5 +2,18 @@ package revision;
 
 public class PerfectNumbers {
     
-    
+    public int perfectNo(int N){
+
+        int sum=0;
+
+        for(int i=1; i<N; i++){
+            if(N%i==0){
+                sum=sum+i;
+            }
+        }
+        if(sum==N){
+            return 1;
+        }
+        return 0;
+    }
 }
