548e2aa57a07378017fd3e26926365a756190ee6
Muhammed <faraanshaik19@gmail.com>
2026-03-04 00:22:25 +0530

    problem bug fix commit

--- revision/PerfectNumbers.java
+++ revision/PerfectNumbers.java
@@ -8,9 +8,34 @@ public class PerfectNumbers {
 
         int sum=0;
 
-        for(int i=1; i<N; i++){
+        for(int i=1; i*i<=N; i++){
             if(N%i==0){
-                sum=sum+i;
+                if(i==N/i){
+                    sum+=i;
+                }else{
+                    sum=sum+(i+N/i);
+                }
+            }
+        }
+        if(sum==N){
+            return 1;
+        }
+        return 0;
+    }
+
+    // TC: O(root N)
+    // SC: O(1)
+    public int perfectNoOptmized(int N){
+
+        int sum=1;
+
+        for(int i=2; i*i<=N; i++){
+            if(N%i==0){
+                if(i==N/i){
+                    sum+=i;
+                }else{
+                    sum=sum+(i+N/i);
+                }
             }
         }
         if(sum==N){
