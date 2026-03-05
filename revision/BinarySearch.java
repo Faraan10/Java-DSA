9694eed15cad2d85f25c86b7c2f7282640358324
Muhammed <faraanshaik19@gmail.com>
2026-03-06 02:53:19 +0530

    problem commit

---
+++ revision/BinarySearch.java
@@ -0,0 +1,32 @@
+package revision;
+import java.util.*;
+
+public class BinarySearch {
+    
+    // finding this key k in the array
+    // TC: O(log N)
+    // SC: O(1)
+    public int binarySearchProblem(int[] A, int K){
+        
+        // A= [1,2,3,4,5,6,7,8]
+        // K= 2
+
+        int low=0;
+        int high=A.length-1;
+        Arrays.sort(A);
+
+        while(low<=high){
+
+            int mid=low+(high-low)/2;
+
+            if(A[mid]==K){
+                return 1;
+            }else if(A[mid]>K){
+                high=mid-1; // high=2
+            }else{
+                low=mid+1;
+            }
+        }
+        return 0;
+    }
+}
