e13ec63df58b6f518462b11711f6c6951f3f590c
Muhammed <faraanshaik19@gmail.com>
2026-03-08 23:32:40 +0530

    string reverse problem commit

---
+++ revision/StringReverse.java
@@ -0,0 +1,33 @@
+package revision;
+
+public class StringReverse {
+    
+    public String solve(String A) {
+
+        int N=A.length();
+        StringBuilder sb=new StringBuilder();
+
+        for(int i=N-1; i>=0; i--){
+
+            while(i>=0 && A.charAt(i)==' '){
+                i--;
+            }
+            if(i<0){
+                break;
+            }
+
+            int end=i;
+            while(i>=0 && A.charAt(i)!=' '){
+                i--;
+            }
+            int start=i+1;
+
+            if(sb.length()>0){
+                sb.append(' ');
+            }
+            sb.append(A.substring(start, end+1));
+        }
+
+        return sb.toString();
+    }
+}
