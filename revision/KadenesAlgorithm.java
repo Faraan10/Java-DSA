536734a178c362469b8da3793ccad6d843a6dd14
Muhammed <faraanshaik19@gmail.com>
2026-03-11 01:32:00 +0530

    optimized problem commit

---
+++ revision/KadenesAlgorithm.java
@@ -0,0 +1,22 @@
+package revision;
+
+public class KadenesAlgorithm {
+    
+    // TC: O(N)
+    // SC: O(1)
+    public int maxSubArray(final int[] A) {
+
+        int N=A.length;
+        int ans=Integer.MIN_VALUE;
+        int sum=0;
+        for(int i=0; i<N; i++){
+
+            sum+=A[i];
+            ans=Math.max(ans, sum);
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return ans;
+    }
+}
