9ee0548ee89a33997501e7f254020e50396f2bcf
Muhammed <faraanshaik19@gmail.com>
2026-03-14 23:08:44 +0530

    problem explanation commit

--- revision/StringReverse.java
+++ revision/StringReverse.java
@@ -12,22 +12,30 @@ public class StringReverse {
 
         for(int i=N-1; i>=0; i--){
 
+            // here it will loop till there are empty spaces if letter found stops
             while(i>=0 && A.charAt(i)==' '){
                 i--;
             }
+            // if i <0 breaks here else out of bounds exception
             if(i<0){
                 break;
             }
 
+            // first getting end pointer
             int end=i;
+            // now going again till where this string ends till we not get
+            // empty space as then i+1 will be start point of string
             while(i>=0 && A.charAt(i)!=' '){
                 i--;
             }
+            // taking that start point
             int start=i+1;
 
+            // as after first string we should add empty space
             if(sb.length()>0){
                 sb.append(' ');
             }
+            // appending string by using substring method 
             sb.append(A.substring(start, end+1));
         }
 
