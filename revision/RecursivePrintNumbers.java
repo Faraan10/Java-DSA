7312e853d86b95803bceffdd89a8d75b3d24b03a
Muhammed <faraanshaik19@gmail.com>
2026-03-19 23:38:56 +0530

    problem commit

---
+++ revision/RecursivePrintNumbers.java
@@ -0,0 +1,21 @@
+package revision;
+
+public class RecursivePrintNumbers {
+
+    // input: 8
+    // output: 1,2,3,4,5,6,7,8
+    public void numbersPrinting(int N){
+
+        if(N==0){
+            return;
+        }
+        numbersPrinting(N-1);
+        System.out.print(N+" ");
+
+    }
+    public void printNo(int N){
+
+        numbersPrinting(N);
+
+    }
+}
