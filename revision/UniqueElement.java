5ac71b4010f974ec8de4b718fd0ede3d10447901
Muhammed <faraanshaik19@gmail.com>
2026-03-25 23:47:04 +0530

    problem commit

---
+++ revision/UniqueElement.java
@@ -0,0 +1,14 @@
+package revision;
+
+public class UniqueElement {
+    
+    public int unique(int[] A){
+
+        int N=A.length;
+        int ans=A[0];
+        for(int i=1 ;i<N; i++){
+            ans=ans^A[i];
+        }
+        return ans;
+    }
+}
