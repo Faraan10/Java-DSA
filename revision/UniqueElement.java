764a505c86b8bbc2a9c1028b50215332f9a2988c
Muhammed <faraanshaik19@gmail.com>
2026-03-25 23:55:42 +0530

    problem commit with explanation

--- revision/UniqueElement.java
+++ revision/UniqueElement.java
@@ -4,7 +4,9 @@ public class UniqueElement {
     
     // TC: O(N)
     // SC: O(1)
-    // inout: [2,3,4,5,3,2]
+    // input: [2,3,4,5,3,5,2] this method only works when there is only 1
+    // unique element in array and all other elements appear exactly 2 times
+    // also first convert into binary then do dry run 
     public int unique(int[] A){
 
         int N=A.length;
