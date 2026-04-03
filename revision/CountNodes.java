ca6bcc9b259a3556eb0800632916c52a7800d87a
Muhammed <faraanshaik19@gmail.com>
2026-04-03 17:08:26 +0530

    problem commit

---
+++ revision/CountNodes.java
@@ -0,0 +1,38 @@
+package revision;
+
+public class CountNodes {
+    
+    class NodeOfTree{
+
+        int val;
+        NodeOfTree left;
+        NodeOfTree right;
+
+        NodeOfTree(int x){
+            val=x;
+            left=null;
+            right=null;
+        }
+    }
+
+    // TC: O(N)
+    // SC: O(h) max height of tree
+    public int NodeCount(NodeOfTree A){
+
+        if(A==null){
+            return 0;
+        }
+        return solve(A);
+    }
+
+    public int solve(NodeOfTree root){
+
+        if(root==null){
+            return 0;
+        }
+        
+        int left=solve(root.left);
+        int right=solve(root.right);
+        return left+right+1;
+    }
+}
