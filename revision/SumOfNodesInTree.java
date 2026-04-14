c273c9ae45a515f06f58c1a07efbb6cee6ed52be
Muhammed <faraanshaik19@gmail.com>
2026-04-14 18:00:38 +0530

    problem commit

--- revision/SumOfNodesInTree.java
+++ revision/SumOfNodesInTree.java
@@ -32,4 +32,27 @@ public class SumOfNodesInTree {
         int right=solve(root.right);
         return left+right+root.val;
     }
+
+
+
+
+
+    // TC: O(N)
+    // SC: O(H)
+    public int sum(TreeNode A){
+
+        return sumNodes(A);
+    }
+
+    public int sumNodes(TreeNode root){
+
+        if(root==null){
+            return 0;
+        }
+
+        int left=sumNodes(root.left);
+        int right=sumNodes(root.right);
+
+        return left+right+root.val;
+    }
 }
