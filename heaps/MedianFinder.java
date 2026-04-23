3f3942eba66f064581e5e91d266c6a5db6a27dee
Muhammed <faraanshaik19@gmail.com>
2026-04-24 03:04:24 +0530

    problem commit

---
+++ heaps/MedianFinder.java
@@ -0,0 +1,45 @@
+package heaps;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class MedianFinder {
+
+    PriorityQueue<Integer> maxHeap;
+    PriorityQueue<Integer> minHeap;
+    public MedianFinder() {
+        maxHeap=new PriorityQueue<>(Collections.reverseOrder());
+        minHeap=new PriorityQueue<>();
+    }
+    
+    public void addNum(int num) {
+        
+        if(maxHeap.isEmpty() || num<=maxHeap.peek()){
+            maxHeap.add(num);
+        }else{
+            minHeap.add(num);
+        }
+
+        if(maxHeap.size()>minHeap.size()+1){
+            minHeap.add(maxHeap.poll());
+        }else if(minHeap.size()>maxHeap.size()){
+            maxHeap.add(minHeap.poll());
+        }
+    }
+    
+    public double findMedian() {
+
+        if(maxHeap.size()==minHeap.size()){
+            return ((double)maxHeap.peek()+minHeap.peek())/2.0;
+        }else{
+            return maxHeap.peek();
+        }
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
