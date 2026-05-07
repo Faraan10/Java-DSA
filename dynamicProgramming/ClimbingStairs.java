d997b0aa359439e6464de57bf8deb621ee4c2371
Muhammed <faraanshaik19@gmail.com>
2026-05-07 23:58:47 +0530

    problem commit

---
+++ dynamicProgramming/ClimbingStairs.java
@@ -0,0 +1,31 @@
+package dynamicProgramming;
+
+import java.util.Arrays;
+
+public class ClimbingStairs {
+    
+    public int climbStairs(int A) {
+
+        int[] dp=new int[A+1];
+        Arrays.fill(dp, -1);
+        int ans=solve(A, dp);
+
+        return ans;
+    }
+
+    public int solve(int N, int[] dp){
+        
+        int MOD=1000000007;
+        if(N<=1){
+            return 1;
+        }
+
+        if(dp[N] != -1){
+            return dp[N];
+        }
+
+        dp[N]=(solve(N-1, dp)+solve(N-2, dp))%MOD;
+
+        return dp[N];
+    }
+}
