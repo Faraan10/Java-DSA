21940f23b94cd0e3044834b38a6b62c401bf7f22
Muhammed <faraanshaik19@gmail.com>
2026-05-24 21:54:49 +0530

    problem commit

---
+++ knapsack/Knapsack012.java
@@ -0,0 +1,50 @@
+package knapsack;
+
+import java.util.Arrays;
+
+public class Knapsack012 {
+    
+    public int solve(int[] A, int[] B, int C) {
+
+        int N = A.length;
+
+        int maxValue = 0;
+
+        for (int val : A) {
+            maxValue += val;
+        }
+
+        long INF = (long)1e18;
+
+        long[] dp = new long[maxValue + 1];
+
+        Arrays.fill(dp, INF);
+
+        dp[0] = 0;
+
+        for (int i = 0; i < N; i++) {
+
+            int value = A[i];
+            int weight = B[i];
+
+            // backward for 0-1 property
+            for (int v = maxValue; v >= value; v--) {
+
+                dp[v] = Math.min(
+                    dp[v],
+                    weight + dp[v - value]
+                );
+            }
+        }
+
+        // largest achievable value
+        for (int v = maxValue; v >= 0; v--) {
+
+            if (dp[v] <= C) {
+                return v;
+            }
+        }
+
+        return 0;
+    }
+}
