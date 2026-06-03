509def872a5bedfd5bb443f218c0d867cd1e2716
Muhammed <faraanshaik19@gmail.com>
2026-06-03 23:44:02 +0530

    optimized problem solution commit

--- AbasicProblemsList/PerfectNumbers.java
+++ AbasicProblemsList/PerfectNumbers.java
@@ -2,7 +2,7 @@ package AbasicProblemsList;
 
 public class PerfectNumbers {
     
-    // TC: O(N)
+    // TC: O(A)
     // SC: O(1)
     public int solve(int A) {
 
@@ -10,6 +10,30 @@ public class PerfectNumbers {
         for(int i=1; i<A; i++){
             if(A%i==0){
                 count+=i;
+            }
+        }
+        if(count==A){
+            return 1;
+        }
+        return 0;
+    }
+
+
+    // TC: O(root A)
+    // SC: O(1)
+    public int optSolve(int A) {
+
+        if(A<=1){
+            return 0;
+        }
+        int count=1;
+        for(int i=2; i*i<=A; i++){
+            if(A%i==0){
+                if(i==A/i){
+                    count=count+i;
+                }else{
+                    count=count+(i+A/i);
+                }
             }
         }
         if(count==A){
