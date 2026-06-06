4ee323288742269e485094951b9273f32ccd1896
Muhammed <faraanshaik19@gmail.com>
2026-06-07 03:12:40 +0530

    explaination on TC commit

--- AbasicProblemsList/SubArrayGivenRange.java
+++ AbasicProblemsList/SubArrayGivenRange.java
@@ -2,7 +2,7 @@ package AbasicProblemsList;
 
 public class SubArrayGivenRange{
 
-    // TC: O(N)
+    // TC: O(N) as in worst case B can be 0 and C can be N-1
     // SC: O(1)
     public int[] solve(int[] A, int B, int C) {
 
