5899be2d7fabfa099b8ae5e63b7ace72e6dace28
Muhammed <faraanshaik19@gmail.com>
2026-06-07 03:11:09 +0530

    problem commit

---
+++ AbasicProblemsList/SubArrayGivenRange.java
@@ -0,0 +1,16 @@
+package AbasicProblemsList;
+
+public class SubArrayGivenRange{
+
+    // TC: O(N)
+    // SC: O(1)
+    public int[] solve(int[] A, int B, int C) {
+
+        int[] arr=new int[C-B+1];
+        
+        for(int i=B; i<=C; i++){
+            arr[i-B]=A[i];
+        }
+        return arr;
+    }
+}
