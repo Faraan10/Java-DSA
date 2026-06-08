7c1c99786dc93945cab866bf96491b0944160fa1
Muhammed <faraanshaik19@gmail.com>
2026-06-09 00:26:08 +0530

    problem commit with optimized solution and updated TC and SC

--- AbasicProblemsList/ClosestMinMax.java
+++ AbasicProblemsList/ClosestMinMax.java
@@ -37,4 +37,54 @@ public class ClosestMinMax {
         }
         return ans;
     }
+
+
+    // optimized problem solution with calculating
+    // max and min first and then using index manipulation
+    // and storing latest min and max index solved it
+    // TC: O(N)
+    // SC: O(1)
+    public int optimizedSolve(int[] A) {
+
+        int N=A.length;
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        int ans=Integer.MAX_VALUE;
+
+        for(int i=0; i<N; i++){
+            if(A[i]>max){
+                max=A[i];
+            }
+            if(A[i]<min){
+                min=A[i];
+            }
+        }
+
+        // edge case as if all elements are same 
+        // or if it is only 1 number in array
+        if(max==min){
+            return 1;
+        }
+
+        int minIdx=-1;
+        int maxIdx=-1;
+        for(int i=0; i<N; i++){
+            
+            if(A[i]==max){
+                maxIdx=i;
+                if(minIdx != -1){
+                    ans=Math.min(ans, (Math.abs(minIdx-maxIdx)+1));
+                }
+            }
+
+            if(A[i]==min){
+                minIdx=i;
+                if(maxIdx != -1){
+                    ans=Math.min(ans, (Math.abs(minIdx-maxIdx)+1));
+                }
+            }
+
+        }
+        return ans;
+    }
 }
