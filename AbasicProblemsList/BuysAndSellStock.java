97635e5a69d3fee6a6549e56520b9fbeb02a89da
Muhammed <faraanshaik19@gmail.com>
2026-06-13 17:15:18 +0530

    problem commit

---
+++ AbasicProblemsList/BuysAndSellStock.java
@@ -0,0 +1,23 @@
+package AbasicProblemsList;
+
+public class BuysAndSellStock {
+    
+    // TC: O(N)
+    // SC: O(1)
+    public int maxProfit(final int[] A) {
+
+        if(A.length==0){
+            return 0;
+        }
+        int N=A.length;
+        int min=A[0];
+        int profit=0;
+        int maxProfit=0;
+        for(int i=1; i<N; i++){
+            profit=A[i]-min;
+            maxProfit=Math.max(profit, maxProfit);
+            min=Math.min(min, A[i]);
+        }
+        return maxProfit;
+    }
+}
