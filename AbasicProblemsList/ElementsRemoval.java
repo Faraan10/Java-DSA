96960840adee1cd24e430e6a33b32c72f3f82994
Muhammed <faraanshaik19@gmail.com>
2026-06-15 22:47:02 +0530

    problem commit

--- AbasicProblemsList/ElementsRemoval.java
+++ AbasicProblemsList/ElementsRemoval.java
@@ -24,4 +24,21 @@ public class ElementsRemoval {
         }
         return sum;
     }
+
+
+    // I tried to multiply the number with how many times 
+    // it was coming in the array and adding its sum
+    
+    // TC:O(N log N)
+    // SC: O(1)
+    public int differentMethodSsolve(int[] A) {
+
+        int N=A.length;
+        Arrays.sort(A);
+        int sum=0;
+        for(int i=0; i<N; i++){
+            sum=sum+(A[i]*(N-i));
+        }
+        return sum;
+    }
 }
