d5d09fa8fc7dc74d32fb08f127deda233c6df3c9
Muhammed <faraanshaik19@gmail.com>
2026-06-15 22:31:16 +0530

    problem solved

---
+++ AbasicProblemsList/ElementsRemoval.java
@@ -0,0 +1,25 @@
+package AbasicProblemsList;
+
+import java.util.Arrays;
+
+public class ElementsRemoval {
+    
+    public int solve(int[] A) {
+
+        int N=A.length;
+        Arrays.sort(A);
+        int sum=0;
+
+        // for(int i=N-1; i>=0; i--){
+        //     for(int j=i; j>=0; j--){
+        //         sum+=A[j];
+        //     }
+        // }
+        // return sum;
+
+        for(int i=0; i<N; i++){
+            sum=sum+((i+1)*A[N-i-1]);
+        }
+        return sum;
+    }
+}
