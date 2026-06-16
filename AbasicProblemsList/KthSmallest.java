0f3bb8c4949b6fa7c6d116f1459dac4c68f2f7ca
Muhammed <faraanshaik19@gmail.com>
2026-06-16 22:24:32 +0530

    problem commit

---
+++ AbasicProblemsList/KthSmallest.java
@@ -0,0 +1,15 @@
+package AbasicProblemsList;
+
+import java.util.Arrays;
+
+public class KthSmallest {
+    
+    // TC: O(N log N)
+    // SC: O(1)
+    public int kthsmallest(final int[] A, int B) {
+
+        Arrays.sort(A);
+
+        return A[B-1];
+    }
+}
