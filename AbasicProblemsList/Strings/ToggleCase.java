8c22b64e9d06113265287e7933c7458fe81e87bb
Muhammed <faraanshaik19@gmail.com>
2026-06-17 02:17:34 +0530

    string problem commit

---
+++ AbasicProblemsList/Strings/ToggleCase.java
@@ -0,0 +1,24 @@
+package AbasicProblemsList.Strings;
+
+public class ToggleCase {
+    
+    // TC: O(N^2) // becaause everytime we create new string 
+    // when appening characters so loop O(N) and string 
+    // creation O(N)
+    // SC: O(1)
+    public String solve(String A) {
+
+        int N=A.length();
+        String str="";
+
+        for(int i=0; i<N; i++){
+            char ch=A.charAt(i);
+            if(ch>='a' && ch<='z'){
+                str=str+(char)(ch-32);
+            }else{
+                str=str+(char)(ch+32);
+            }
+        }
+        return str;
+    }
+}
