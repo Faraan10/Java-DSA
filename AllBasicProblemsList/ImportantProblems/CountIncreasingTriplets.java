764cb00edd043f62652e5d667ed18865ff22f234
Muhammed <faraanshaik19@gmail.com>
2026-06-22 00:07:21 +0530

    problem brute force approach

---
+++ AllBasicProblemsList/ImportantProblems/CountIncreasingTriplets.java
@@ -0,0 +1,25 @@
+package AllBasicProblemsList.ImportantProblems;
+
+public class CountIncreasingTriplets {
+    
+    
+    public int solve(int[] A) {
+
+
+        // TC: O(N^3)
+        // SC: O(1)
+        int N=A.length;
+        int count=0;
+
+        for(int i=0; i<N; i++){
+            for(int j=i+1; j<N; j++){
+                for(int k=j+1; k<N; k++){
+                    if(A[i]<A[j] && A[j]<A[k]){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
