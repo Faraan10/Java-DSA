4c52914cd9da514561c5eb890e37c087324a673d
Muhammed <faraanshaik19@gmail.com>
2026-06-21 23:52:37 +0530

    majority element problem

---
+++ AllBasicProblemsList/ImportantProblems/MajorityElement.java
@@ -0,0 +1,78 @@
+package AllBasicProblemsList.ImportantProblems;
+
+public class MajorityElement {
+    
+    public int majorityElement(final int[] A) {
+
+        // TC: O(N)
+        // SC: O(N)
+        // int N=A.length;
+        // HashMap<Integer, Integer> hm=new HashMap<>();
+
+        // for(int i=0; i<N; i++){
+        //     hm.put(A[i], hm.getOrDefault(A[i], 0)+1);
+        // }
+
+        // int majority=N/2;
+        // for(int i=0; i<N; i++){
+        //     if(hm.get(A[i])>majority){
+        //         return A[i];
+        //     }
+        // }
+        // return 0;
+
+        // TC: O(N log N)
+        // SC: O(1)
+        // int N=A.length;
+        // int count=1;
+        // int majority=N/2;
+
+        // Arrays.sort(A);
+        // // if(N==1){
+        // //     return A[0];
+        // // }
+
+        // for(int i=1; i<N; i++){
+        //     if(A[i-1]==A[i]){
+        //         count++;
+        //     }else{
+        //         count=1;
+        //     }
+        //     if(count>majority){
+        //         return A[i];
+        //     }
+        // }
+        // return A[0];
+
+
+        // Moores voting algorithm
+
+        int N=A.length;
+        int majority=A[0];
+        int freq=1;
+
+        for(int i=1; i<N; i++){
+
+            if(majority==A[i]){
+                freq++;
+            }else{
+                freq--;
+            }
+            if(freq==0){
+                majority=A[i];
+                freq=1;
+            }
+        }
+
+        int count=0;
+        for(int i=0; i<N; i++){
+            if(A[i]==majority){
+                count++;
+            }
+        }
+        if(count>A.length/2){
+            return majority;
+        }
+        return A[0];
+    }
+}
