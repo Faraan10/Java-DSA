1da22070fa28a6b55423bd2c9bc66e073b257d56
Muhammed <faraanshaik19@gmail.com>
2026-07-02 23:05:26 +0530

    problem with more clean syntax and better version

--- DsaInterviewProblems/MeetingRooms.java
+++ DsaInterviewProblems/MeetingRooms.java
@@ -32,4 +32,29 @@ public class MeetingRooms {
         }
         return pq.size();
     }
+
+
+    // same problem small changes more safety check and avoiding 
+    // else case
+    public int varsolve(int A, int[][] B) {
+
+        Arrays.sort(B, (x,y)-> Integer.compare(x[0], y[0])); // sorting based on 
+        // start times
+        
+        if(A==0){
+            return 0;
+        }
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        pq.add(B[0][1]); // adding first end time into heap
+        for(int i=1; i<A; i++){
+            int start=B[i][0];
+            int end=B[i][1];
+
+            if(!pq.isEmpty() && start>=pq.peek()){
+                pq.poll();
+            }
+            pq.add(end);
+        }
+        return pq.size();
+    }
 }
