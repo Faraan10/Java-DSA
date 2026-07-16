6b04b2178d831134cbaf0f482bbf5ae7d2e37312
Muhammed <faraanshaik19@gmail.com>
2026-07-16 23:34:28 +0530

    problem commit

--- AllBasicProblemsList/ImportantProblems/dp/HouseRobber.java
+++ AllBasicProblemsList/ImportantProblems/dp/HouseRobber.java
@@ -6,6 +6,7 @@ public class HouseRobber {
     
     // TC: O(N)
     // SC: O(N)
+    // Top down approach (recursive approach) with DP Memoization
     public int rob(int[] nums) {
 
         int N=nums.length;
@@ -33,5 +34,30 @@ public class HouseRobber {
 
         dp[i]=ans;
         return dp[i];
+    }
+
+
+    // TC: O(N)
+    // SC: O(1)
+    // Bottom up approach (iterative approach) with DP tabulation
+    public int robItr(int[] nums) {
+        
+        int N=nums.length;
+        if(N==0){
+            return 0;
+        }
+
+        if(N==1){
+            return nums[0];
+        }
+
+        int[] dp=new int[N];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0], nums[1]);
+
+        for(int i=2; i<N; i++){
+            dp[i]=Math.max(nums[i]+dp[i-2], dp[i-1]);
+        }
+        return dp[N-1];
     }
 }
