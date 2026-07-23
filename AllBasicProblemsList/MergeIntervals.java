38cab3a486ffa648b3a4efea0ea069866434e736
Muhammed <faraanshaik19@gmail.com>
2026-07-23 23:54:10 +0530

    problem commit

---
+++ AllBasicProblemsList/MergeIntervals.java
@@ -0,0 +1,16 @@
+package AllBasicProblemsList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MergeIntervals {
+    
+    public ArrayList<ArrayList<Integer>> solve(ArrayList<ArrayList<Integer>> A) {
+
+        ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
+
+        Collections.sort(A, (x, y)-> Integer.compare(x.get(0), y.get(0)));
+
+        int s1=
+    }
+}
