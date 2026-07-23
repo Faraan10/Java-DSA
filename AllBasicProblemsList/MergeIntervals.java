5b0525bc94f6bde1872d2faa00105ed9e5a46cc9
Muhammed <faraanshaik19@gmail.com>
2026-07-24 00:00:27 +0530

    problem commit with TC and SC

--- AllBasicProblemsList/MergeIntervals.java
+++ AllBasicProblemsList/MergeIntervals.java
@@ -5,12 +5,41 @@ import java.util.Collections;
 
 public class MergeIntervals {
     
+    // TC: O(N log N)
+    // SC: O(N)
     public ArrayList<ArrayList<Integer>> solve(ArrayList<ArrayList<Integer>> A) {
 
+        int N=A.size();
         ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
 
         Collections.sort(A, (x, y)-> Integer.compare(x.get(0), y.get(0)));
 
-        int s1=
+        int s1=A.get(0).get(0);
+        int e1=A.get(0).get(1);
+
+        for(int i=1; i<N; i++){
+
+            int s2=A.get(i).get(0);
+            int e2=A.get(i).get(1);
+
+            if(e1>=s2){
+                e1=Math.max(e1, e2);
+            }else{
+
+                ArrayList<Integer> list=new ArrayList<>();
+                list.add(s1);
+                list.add(e1);
+                ans.add(list);
+
+                s1=s2;
+                e1=e2;
+            }
+        }
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(s1);
+        list.add(e1);
+        ans.add(list);
+
+        return ans;
     }
 }
