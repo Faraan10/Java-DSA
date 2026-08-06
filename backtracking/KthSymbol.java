b9fc6ddd4b0c1671a84ac4a4545008ac4444affe
Muhammed <faraanshaik19@gmail.com>
2026-08-06 22:00:05 +0530

    problem commit iterative approach

--- backtracking/KthSymbol.java
+++ backtracking/KthSymbol.java
@@ -7,26 +7,4 @@ public class KthSymbol {
     public int solve(int A, int B) {
         return Integer.bitCount(B) % 2;
     }
-
-    // TC: O(A)
-    // SC: O(A)
-    public int backtrackSolve(int A, int B) {
-
-        return helper(A, B, 0);
-    }
-
-    private int helper(int row, int index, int value) {
-        if (row == 1)
-            return value;
-
-        int half = 1 << (row - 2);
-
-        if (index < half) {
-            // Left child
-            return helper(row - 1, index, value);
-        } else {
-            // Right child
-            return helper(row - 1, index - half, 1 - value);
-        }
-    }
 }
