8ddb090ea0fad6e8a35a46994bf57008fff9e940
Muhammed <faraanshaik19@gmail.com>
2026-08-30 23:49:08 +0530

    problem commit

---
+++ trees/binaryTrees/SerializeBinaryTree.java
@@ -0,0 +1,34 @@
+package trees.binaryTrees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SerializeBinaryTree {
+    
+    // TC: O(N)
+    // SC: O(N)
+    public ArrayList<Integer> solve(TreeNode A) {
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.add(A);
+
+        while (!queue.isEmpty()) {
+
+            TreeNode curr = queue.poll();
+
+            if (curr == null) {
+                ans.add(-1);
+            } else {
+                ans.add(curr.val);
+
+                queue.add(curr.left);
+                queue.add(curr.right);
+            }
+        }
+
+        return ans;
+    }
+}
